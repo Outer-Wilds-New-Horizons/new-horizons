1347f3fb06f0bb5b17d33a440ff7a029a2fc20b6
Nick J. Connors <nicholasconnors@hotmail.ca>
2022-01-19 22:38:51 -0500

    Fixed camera far clip plane being too big for shaders

--- NewHorizons/Tools/OWCameraFix.cs
+++ NewHorizons/Tools/OWCameraFix.cs
@@ -17,7 +17,7 @@ namespace NewHorizons.Tools
         private static void OnOWCameraAwake(OWCamera __instance)
         {
             var newDist = __instance.farClipPlane * 10f;
-            if (__instance.useFarCamera) newDist = Mathf.Min(500000f, newDist);
+            newDist = Mathf.Clamp(newDist, 500000f, 10000000f);
             __instance.farClipPlane = newDist;
             __instance.farCameraDistance = newDist;
         }
