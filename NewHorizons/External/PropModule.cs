345f5a916969349174113b1cb6942c21d7c42cb3
Nick J. Connors <nicholasconnors@hotmail.ca>
2022-02-20 01:14:33 -0500

    Set default values for RevealInfo

--- NewHorizons/External/PropModule.cs
+++ NewHorizons/External/PropModule.cs
@@ -72,9 +72,9 @@ namespace NewHorizons.External
 
         public class RevealInfo
         {
-            public string revealOn;
+            public string revealOn = "enter";
             public string[] reveals;
-            public MVector3 position;
+            public MVector3 position = new MVector3(0, 0, 0);
             public float radius = 1f;
             public float maxDistance = -1f; // Snapshot & Observe Only
             public float maxAngle = 180f; // Observe Only
