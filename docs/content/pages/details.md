2552d1b8a498a430dbbb4183afa13212c3d15be9
Ben C <bwc9876@gmail.com>
2022-03-04 23:27:26 -0800

    Added XML Schemas To Docs (#57)
    * Add Bootstrap Extension
    
    * Rename main.yml
    
    * Artifact Upload
    
    * Fix Bootstrap Reference Error
    
    * BootstrapTreeProcessor
    
    * getiterator removed
    
    * keys function
    
    * Style Images
    
    * Update docs_build.yml
    
    * Added Meta Files
    
    * Template Get
    
    * Fix Page Ref
    
    * Update BASE_URL
    
    * Sort Schemas
    
    * Add Sitemaps
    
    * Add favicons, open-graph, and setup guide
    
    * Update Setup.md
    
    * Update .gitignore
    
    * Update Setup.md
    
    * Use _blank on external links
    
    * Restructured Docs
    
    * Fix Links
    
    * Added XML Schemas
    
    * Name XML Schemas

--- docs/content/pages/details.md
+++ docs/content/pages/details.md
@@ -3,20 +3,20 @@ Sort-Priority: 90
 
 ## Details/Scatterer
 
-For physical objects there are currently two ways of setting them up: specify an asset bundle and path to load a custom asset you created, or specify the path to the item you want to copy from the game in the scene hierarchy. Use the [Unity Explorer](https://outerwildsmods.com/mods/unityexplorer) mod to find an object you want to copy onto your new body. Some objects work better than others for this. Good luck. Some pointers:
+For physical objects there are currently two ways of setting them up: specify an asset bundle and path to load a custom asset you created, or specify the path to the item you want to copy from the game in the scene hierarchy. Use the [Unity Explorer](https://outerwildsmods.com/mods/unityexplorer){ target="_blank" } mod to find an object you want to copy onto your new body. Some objects work better than others for this. Good luck. Some pointers:
 - Use "Object Explorer" to search
-- Do not use the search functionality on Scene Explorer, it is really really slow. Use the "Object Search" tab instead.
+- Do not use the search functionality on Scene Explorer, it is really, really slow. Use the "Object Search" tab instead.
 - Generally you can find planets by writing their name with no spaces/punctuation followed by "_Body".
 
 ## Asset Bundles
 
-Here is a template project: [Outer Wilds Unity Template](https://github.com/xen-42/outer-wilds-unity-template)
+Here is a template project: [Outer Wilds Unity Template](https://github.com/xen-42/outer-wilds-unity-template){ target="_blank" }
 
 The template project contains ripped versions of all the game scripts, meaning you can put things like DirectionalForceVolumes in your Unity project to have artificial gravity volumes loaded right into the game.
 
 If for whatever reason you want to set up a Unity project manually instead of using the template, follow these instructions:
 
-1. Start up a Unity 2017 project (I use Unity 2017.4.40f1 (64-bit), so if you use something else I can't guarantee it will work). The DLC updated Outer Wilds to 2019.4.27 so that probably works but I personally haven't tried it.
+1. Start up a Unity 2017 project (I use Unity 2017.4.40f1 (64-bit), so if you use something else I can't guarantee it will work). The DLC updated Outer Wilds to 2019.4.27 so that probably works, but I personally haven't tried it.
 2. In the "Assets" folder in Unity, create a new folder called "Editor". In it create a file called "CreateAssetBundle.cs" with the following code in it:
 
 ```cs
@@ -40,8 +40,8 @@ public class CreateAssetBundles
 ```
 
 3. Create your object in the Unity scene and save it as a prefab.
-4. Add all files used (models, prefabs, textures, materials, etc) to an asset bundle by selecting them and using the drop down in the bottom right. Here I am adding a rover model to my "rss" asset bundle for the Real Solar System add-on.
+4. Add all files used (models, prefabs, textures, materials, etc.) to an asset bundle by selecting them and using the dropdown in the bottom right. Here I am adding a rover model to my "rss" asset bundle for the Real Solar System add-on.
 ![setting asset bundle]({{ 'images/detailing/asset_bundle.png'|static }})
 
 5. In the top left click the "Assets" drop-down and select "Build AssetBundles". This should create your asset bundle in a folder in the root directory called "StreamingAssets".
-6. Copy the asset bundle and asset bundle .manifest files from StreamingAssets into your mod's "planets" folder. If you did everything properly they should work in game. To double check everything is included, open the .manifest file in a text editor to see the files included and their paths.
+6. Copy the asset bundle and asset bundle .manifest files from StreamingAssets into your mod's "planets" folder. If you did everything properly they should work in game. To double-check everything is included, open the .manifest file in a text editor to see the files included and their paths.
--- docs/generate.py
+++ docs/generate.py
@@ -2,102 +2,76 @@ import os
 from pathlib import Path
 from shutil import copytree, rmtree
 
-from htmlmin import minify
 from jinja2 import Environment, select_autoescape, FileSystemLoader
-from markupsafe import Markup
-from json_schema_for_humans.generate import GenerationConfiguration
-from markdown import Markdown
 
-from lib.BootstrapExtension import BootstrapExtension
-from lib.Schema import Schema
-from lib.Page import Page
+from lib.Content.HTMLPage import HTMLPage
+from lib.Content.JSONSchema import JSONSchema
+from lib.Content.MDPage import MDPage
+from lib.Content.MetaItem import MetaItem, MinifiedMetaItem
+from lib.Content.XMLSchema import XMLSchema
+
+print("Setup")
 
 OUT_DIR = os.getenv("OUT_DIR", "/")
 BASE_URL = os.getenv("BASE_URL", "")
+
+env = Environment(
+    loader=FileSystemLoader("content/"),
+    autoescape=select_autoescape(['jinja2'])
+)
+
+scanners = (MDPage, HTMLPage)
+schema_scanners = (JSONSchema, XMLSchema)
+meta_files = (MinifiedMetaItem(Path('browserconfig.jinja2'), '.xml'), MinifiedMetaItem(Path('sitemap.jinja2'), '.xml'), MetaItem(Path('robots.jinja2'), '.txt'))
+
+router = {}
+
+env.filters.update({
+    'upper_first': lambda x:   x[0].upper() + x[1:],
+    'static': lambda path: str(Path(OUT_DIR, path).as_posix()),
+    'route': lambda title:   router.get(title.lower(), "#"),
+    'full_url': lambda relative: BASE_URL + (relative[1:] if relative[0] == "/" else relative)
+})
+
+MetaItem.initialize(env)
+MinifiedMetaItem.initialize(env)
+
+pages = []
+schemas = []
+
+print("Clearing Old Output")
 
 if Path("out/").exists():
     rmtree("out/", ignore_errors=True)
 
 copytree("content/static", "out")
-os.makedirs("out/schemas", exist_ok=True)
 
-env = Environment(
-    loader=FileSystemLoader("content"),
-    autoescape=select_autoescape(['jinja2'])
-)
+print("Scanning For Files")
 
-markdown_settings = {
-    'extensions': ['extra', 'toc', 'meta', BootstrapExtension()]
-}
+for scanner in scanners:
+    new_pages = scanner.initialize(env)
+    for page in new_pages:
+        page.add_route(router, OUT_DIR)
+    pages += new_pages
 
-schema_settings = GenerationConfiguration(custom_template_path="content/base/schema_base.jinja2")
-schema_settings.link_to_reused_ref = False
-schema_settings.minify = True
-
-minify_settings = {
-    'remove_empty_space': True,
-    'keep_pre': True,
-    'remove_optional_attribute_quotes': False
-}
-
-env.minify_settings = minify_settings
-
-md = Markdown(**markdown_settings)
-
-env.filters["upper_first"] = lambda x:   x[0].upper() + x[1:]
-env.filters["markdown"] = lambda text:  Markup(md.convert(text))
-env.filters["static"] = lambda path: str(Path(OUT_DIR, path).as_posix())
-
-pages_paths = Path("content/pages").glob("**/*.md")
-schemas_paths = Path("content/schemas").glob("**/*.json")
-
-router = {}
-
-env.filters['route'] = lambda title:   router.get(title.lower(), "#")
-env.filters['full_url'] = lambda relative: BASE_URL + (relative[1:] if relative[0] == "/" else relative)
-
-pages = []
-schemas = []
-
-for page_path in pages_paths:
-    new_page = Page(page_path, env, markdown_settings)
-    router[new_page.title.lower()] = OUT_DIR + str(new_page.out_path.relative_to('out/'))
-    pages.append(new_page)
-
-for schema_path in schemas_paths:
-    new_schema = Schema(schema_path, env, schema_settings)
-    router[new_schema.title.lower()] = OUT_DIR + "schemas/" + str(new_schema.out_path.relative_to("out/schemas/"))
-    schemas.append(new_schema)
+for scanner in schema_scanners:
+    new_schemas = scanner.initialize(env)
+    for schema in new_schemas:
+        schema.add_route(router, OUT_DIR)
+    schemas += new_schemas
 
 content = pages + schemas
 
-if OUT_DIR != "":
-    router['home'] = OUT_DIR
+print("Generating Pages")
 
 pages.sort(key=lambda p: p.sort_priority, reverse=True)
 schemas.sort(key=lambda s: s.title)
 
-
-def log_build(in_path, out_path):
-    print("Building:", str(in_path), "->", str(out_path))
-
-
-def build_meta(in_path, out_path, do_minify=False):
-    log_build(in_path, out_path)
-    meta_template = env.get_template(str(in_path.relative_to("content/")))
-    with Path("out/", out_path).open(mode="w+", encoding="utf-8") as file:
-        out = meta_template.render(content=content)
-        if do_minify:
-            out = minify(out, **minify_settings)
-        file.write(out)
-
-
-print("Building Meta Files")
-build_meta(Path("content/sitemap.jinja2"), Path("sitemap.xml"), do_minify=True)
-build_meta(Path("content/robots.jinja2"), Path("robots.txt"))
-build_meta(Path("content/browserconfig.jinja2"), Path("fav/browserconfig.xml"), do_minify=True)
-
-print("Building Pages")
 for item in content:
-    log_build(item.in_path, item.out_path)
-    item.render(page=item, pages=pages, schemas=schemas)
+    item.generate(pages=pages, schemas=schemas)
+
+for meta_file in meta_files:
+    meta_file.env = env
+    meta_file.generate(content=content)
+
+print("Done")
