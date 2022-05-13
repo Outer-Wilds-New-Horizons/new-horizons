3714477046f4d5448016878302102c925e03b493
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-05-13 09:57:13 -0400

    feat: added references to DebugRaycaster and DebugPropPlacer, added text field to set current object

--- NewHorizons/Utility/DebugMenu.cs
+++ NewHorizons/Utility/DebugMenu.cs
@@ -8,15 +8,63 @@ using UnityEngine.InputSystem;
 
 namespace NewHorizons.Utility
 {
+    [RequireComponent(typeof(DebugRaycaster))]
+    [RequireComponent(typeof(DebugPropPlacer))]
     class DebugMenu : MonoBehaviour
     {
         GUIStyle _editorMenuStyle;
         Vector2 EditorMenuSize = new Vector2(600, 900);
         bool menuOpen = false;
 
+        DebugPropPlacer _dpp;
+        DebugRaycaster _drc;
+
+        private void Awake()
+        {  
+            _dpp = this.GetRequiredComponent<DebugPropPlacer>();
+            _drc = this.GetRequiredComponent<DebugRaycaster>();
+        }
+
+        private void Update()
+        {
+            if (!Main.Debug) return;
+
+            if (Keyboard.current[Key.Escape].wasPressedThisFrame)
+            {
+                menuOpen = !menuOpen;
+                if (menuOpen) InitMenu();
+            }
+        }
+        
+        private void OnGUI()
+        {
+            if (!menuOpen) return;
+            if (!Main.Debug) return;
+
+            Vector2 menuPosition = Vector2.zero; // new Vector2(Screen.width - EditorMenuSize.x - 10, 10);
+            
+            //TODO: add gui for stuff https://github.com/Bwc9876/OW-SaveEditor/blob/master/SaveEditor/SaveEditor.cs
+            // https://docs.unity3d.com/ScriptReference/GUI.TextField.html
+
+            GUILayout.BeginArea(new Rect(menuPosition.x, menuPosition.y, EditorMenuSize.x, EditorMenuSize.y), _editorMenuStyle);
+            _dpp.currentObject = GUILayout.TextArea(_dpp.currentObject);
+
+            // TODO: maintain list of objects here
+            // TODO: field to provide name of mod to load configs from
+
+            GUILayout.Label("*: Restart Required");
+            GUILayout.Space(20);
+            GUILayout.Label("*: Restart Required");
+            GUILayout.EndArea();
+        }
+
         private void InitMenu()
         {
             if (_editorMenuStyle != null) return;
+            
+            _dpp = this.GetRequiredComponent<DebugPropPlacer>();
+            _drc = this.GetRequiredComponent<DebugRaycaster>();
+
 
             Texture2D bgTexture = MakeTexture((int)EditorMenuSize.x, (int)EditorMenuSize.y, Color.black);
 
@@ -42,33 +90,6 @@ namespace NewHorizons.Utility
             newTexture.SetPixels(pixels);
             newTexture.Apply();
             return newTexture;
-        }
-
-        private void OnGUI()
-        {
-            if (!menuOpen) return;
-            if (!Main.Debug) return;
-
-            Vector2 menuPosition = Vector2.zero; // new Vector2(Screen.width - EditorMenuSize.x - 10, 10);
-            
-            //TODO: add gui for stuff https://github.com/Bwc9876/OW-SaveEditor/blob/master/SaveEditor/SaveEditor.cs
-            // https://docs.unity3d.com/ScriptReference/GUI.TextField.html
-            GUILayout.BeginArea(new Rect(menuPosition.x, menuPosition.y, EditorMenuSize.x, EditorMenuSize.y), _editorMenuStyle);
-            GUILayout.Label("*: Restart Required");
-            GUILayout.Space(20);
-            GUILayout.Label("*: Restart Required");
-            GUILayout.EndArea();
-        }
-
-        private void Update()
-        {
-            if (!Main.Debug) return;
-
-            if (Keyboard.current[Key.Escape].wasPressedThisFrame)
-            {
-                menuOpen = !menuOpen;
-                if (menuOpen) InitMenu();
-            }
         }
     }
 }
