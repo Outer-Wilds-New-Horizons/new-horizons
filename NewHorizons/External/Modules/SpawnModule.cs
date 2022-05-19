ef24245c60c2fee8fc66f5e9596310edef9541de
Nick <nicholasconnors@hotmail.ca>
2022-05-19 17:05:09 -0400

    Custom player/ship rotation upon warp (#128)
    Closes #128

--- NewHorizons/External/Modules/SpawnModule.cs
+++ NewHorizons/External/Modules/SpawnModule.cs
@@ -4,7 +4,9 @@ namespace NewHorizons.External.Modules
     public class SpawnModule
     {
         public MVector3 PlayerSpawnPoint { get; set; }
+        public MVector3 PlayerSpawnRotation { get; set; }
         public MVector3 ShipSpawnPoint { get; set; }
+        public MVector3 ShipSpawnRotation { get; set; }
         public bool StartWithSuit { get; set; }
     }
 }
