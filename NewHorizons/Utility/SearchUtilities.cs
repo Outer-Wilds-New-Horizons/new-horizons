7297f1eb24b372f3c7313292d2212bb9351bd6f8
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-05-21 22:55:50 -0400

    fix: SearchUtilities can now find gameobjects with inactive parents

--- NewHorizons/Utility/SearchUtilities.cs
+++ NewHorizons/Utility/SearchUtilities.cs
@@ -135,7 +135,7 @@ namespace NewHorizons.Utility
 
                     // Get the root object and hope its the right one
                     var root = GameObject.Find(names[0]);
-                    if (root == null) root = FindObjectOfTypeAndName<GameObject>(names[0]);
+                    if (root == null) root = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects().Where(x => x.name.Equals(names[0])).FirstOrDefault();
 
                     var t = root?.transform;
                     if (t == null)
