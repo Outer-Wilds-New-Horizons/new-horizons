8e46372804596a62799be4bb09754ca9241d5ae5
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-05-31 09:44:40 -0400

    added a ton of comments, improved Curiosity validation, and fixed a bug or two

--- docs/content/static/scripts/shiplog_validator.js
+++ docs/content/static/scripts/shiplog_validator.js
@@ -1,142 +1,204 @@
 // Clay made all of this
 
+// I wish this was TypeScript. The code would be so much cleaner and I wouldn't need so many "this returns objects of form X" comments ;-;
 
 // filesContents is an array of the contents of the xml files to validate with
 // throws and exception if the files are not valid xml
-const parse = (filesContents) => {
-    const parser = new DOMParser()
-    const xmlDocs = filesContents.map((contents, index) => {
-            try {
-                return parser.parseFromString(contents, "text/xml")
-            } catch {
-                throw new Error(`File ${index} was unable to be parsed.`)
-            }
-        }
-    )
-    return xmlDocs
+module.exports.parse = (filesContents) => {
+	const parser = new DOMParser()
+	const xmlDocs = filesContents.map((contents, index) => 
+		{
+			try {
+				return parser.parseFromString(contents, "text/xml")
+			} catch {
+				throw new Error(`File ${index} was unable to be parsed.`)
+			}
+		}
+	)
+	return xmlDocs
 }
 
 // throws and exception if the files are invalid
 // otherwise does not
-const validate = (xmlDocs) => {
-    const planetIDs = xmlDocs.map((doc, index) => getChildByTag(doc.documentElement, "ID", `Document ${index} does not have a planet id.`).innerHTML)
-    const allEntryIDs = getAllIDsForTagType(xmlDocs, "Entry")
+module.exports.validate = (xmlDocs) => {
+	const planetIDs = xmlDocs.map((doc, index) => getChildByTag(doc.documentElement, "ID", `Document ${index} does not have a planet id.`).innerHTML)
+	const allEntryIDs = getAllIDsForTagType(xmlDocs, "Entry")
 
-    const allRumorFactIDs = getAllIDsForTagType(xmlDocs, "RumorFact")
-    const allExploreFactIDs = getAllIDsForTagType(xmlDocs, "ExploreFact")
-    const allFactIDs = allExploreFactIDs.concat(allRumorFactIDs)
+	const allRumorFactIDs = getAllIDsForTagType(xmlDocs, "RumorFact")
+	const allExploreFactIDs = getAllIDsForTagType(xmlDocs, "ExploreFact")
+	const allFactIDs = allExploreFactIDs.concat(allRumorFactIDs)
+	
+	// validate id uniqueness
+	checkForDuplicateIDs(allEntryIDs)
+	checkForDuplicateIDs(allFactIDs)
+	
+	checkForDuplicateIDs(allEntryIDs.concat(allFactIDs).concat(planetIDs))
 
-    // validate id uniqueness
-    checkForDuplicateIDs(allEntryIDs)
-    checkForDuplicateIDs(allFactIDs)
+	// validate curiosity references and that all entries referred to as curiosities actually are curiosities
+    validateReferences("Curiosity", allEntryIDs, "Entry")
+        .map(referenceConection => {
+            const curiosityEntry = referenceConection.referencedElement
 
-    checkForDuplicateIDs(allEntryIDs.concat(allFactIDs).concat(planetIDs))
+            // get the ID of the Entry that should be a curiosity, for use in error reporting
+            const curiosityEntryId = reference.referenceID
+            
+            // ensure that this Entry actually is marked as a curiosity
+            getChildByTag(curiosityEntry, "IsCuriosity", `Entry id ${curiosityEntryId} is referred to as a curiosity by an <Entry> in file ${reference.index}, but does not have the <IsCuriosity/> tag.`)
+        }
+    )
 
-    // validate curiosity references and that all entries referred to as curiosities actually are curiosities
-    const allCuriosityReferences = xmlDocs.map((doc, index) => [...doc.getElementsByTagName("Curiosity")].map(element => {
-        return {element, referenceID: element.innerHTML, index}
-    }))
-    allCuriosityReferences
-        .map(reference =>
-            getElementByIDTag(allEntryIDs, reference.referenceID, `An <Entry> in file ${reference.index} has an invalid reference ID for <Curiosity>: "${reference.referenceID}". There is no <Entry> with the supplied ID.`)
-                .getChildByTag(reference.element, "IsCuriosity", `Entry ${reference.referenceID} in file ${reference.index} is referred to as a curiosity by an event in file ${reference.index}, but does not have the <IsCuriosity/> tag.`)
-        )
+	// validate SourceID and Condition references
+	validateReferences("SourceID", allEntryIDs, "Entry")
+	validateReferences("Condition", allFactIDs, "ExploreFact or RumourFact")
+	validateReferences("IgnoreMoreToExploreCondition", allFactIDs, "ExploreFact or RumourFact")
+	
+	
+	// validate that all AltText tags have a <Condition> child
+	allTagsOfTypeHaveChildOfType(xmlDocs, "AltText", "Condition")
 
-    // validate SourceID and Condition references
-    validateReferences("SourceID", allEntryIDs, "Entry")
-    validateReferences("Condition", allFactIDs, "ExploreFact or RumourFact")
-    validateReferences("IgnoreMoreToExploreCondition", allFactIDs, "ExploreFact or RumourFact")
-
-
-    // validate that all AltText tags have a <Condition> child
-    allTagsOfTypeHaveChildOfType(xmlDocs, "AltText", "Condition")
-
-    // validate that all <Entry> tags have a name
-    allTagsOfTypeHaveChildOfType(xmlDocs, "Entry", "Name")
+	// validate that all <Entry> tags have a name
+	allTagsOfTypeHaveChildOfType(xmlDocs, "Entry", "Name")
 }
 
 const validateReferences = (referencerType, possibleReferencees, referenceeType) => {
-    const allReferences = xmlDocs.map((doc, index) => [...doc.getElementsByTagName("SourceID")].map(element => {
-        return {element, referenceID: element.innerHTML, index}
-    }))
-    allReferences
-        .map(reference =>
-            getElementByIDTag(possibleReferencees, referenceID, `A <${referencerType}> tag in file ${reference.index} has an invalid reference id: "${reference.referenceID}". There is no <${referenceeType}> with the supplied ID.`)
-        )
+    // allReferences is an array of all <referencerType> tags: 
+    // [
+    //     {
+    //         element: instance of <referencerType>, 
+    //         referenceID: the text between `element` and its matching closing tag, 
+    //         index: the index of the xml document element is from
+    //     }
+    // ]
+	const allReferences = xmlDocs.map(
+		(doc, index) => 
+		[...doc.getElementsByTagName(referencerType)]
+		.map(element => { 
+				return {element, referenceID: element.innerHTML, index} 
+			}
+		)
+	)
+	.flat()
+
+    // the actual validation
+	const tagsReferenced = allReferences
+	.map(reference => {                                                // for each <referencerType> tag
+            const referencedElement = getElementByIDTag(               // get the tag with the ID it references, where the id it references is <referencerType>THIS IS THE ID</referencerType>
+                possibleReferencees,                                   // note: for most uses of this function, we don't do anything with the referenced tag, we're just making sure it exists, and throwing an error if it doesn't.
+                referenceID, 
+                `A <${referencerType}> tag in file ${reference.index} has an invalid reference id: "${reference.referenceID}". There is no <${referenceeType}> with the supplied ID.`
+            )
+
+            return {referencedElement, reference}
+        }
+	)
+
+    // return the list of referenced tags and the referencepointing to them in case further validation is needed
+    // return value type:
+    // [
+    //      {
+    //          referencedElement: the tag being referenced, eg an <Entry>
+    //          reference: {
+    //              element: the tag doing the referencing, 
+    //              referenceID: the id of the tag being referenced, 
+    //              index: which xml file contains the tag doing the referencing
+    //          }
+    //      }
+    // ]
+    //
+    //
+    return tagsReferenced
 }
 
 const allTagsOfTypeHaveChildOfType = (xmlDocs, tagType, childType) => {
-    xmlDocs.map((doc, index) =>
-        [...doc.getElementsByTagName(tagType)]
-            .map((element, jndex) =>
-                element.getChildByTag(element, childType, `<${tagType}> tag #${jndex} in file ${index} has no <${childType}> tag.`)
-            )
-    )
+	xmlDocs.map((doc, index) =>                // for each document,
+		[...doc.getElementsByTagName(tagType)] // get all <tagType> tags in this document
+		.map((element, jndex) =>               // for each <tagType> in this document
+			element.getChildByTag(             // make sure this tag has at least one <childType> tag as a child
+				element, 
+				childType, 
+				`<${tagType}> tag #${jndex} in file ${index} has no <${childType}> tag.`
+			)
+		)
+	)
 }
 
 const checkForDuplicateIDs = (ids) => {
-    const unique = {}
-    const duplicate = {}
+    // ids is an array returned from getAllIDsForTagType
 
-    for (const id of ids) {
-        if (unique[id.id]) {
-            if (duplicate[id.id]) duplicate[id.id].push(id.docIndex)
-            else duplicate[id.id] = [unique[id.id], id.docIndex]
-        } else {
-            unique[id.id] = id.docIndex
-        }
-    }
+	const unique = {}     // a dictionary of { ID: [index of the doc a tag with this id appears in, ...] }
+	const duplicate = {}  // same as the above. note: the arrays may contain duplicates, ie { "A_COOL_ROCK": [2, 2, 5, 2] }, meaning an element with id "A_COOL_ROCK" appears 3 times in doc #2 and once in doc #5
 
-    let duplications = ""
-    for (const [id, docIndices] of Object.entries(duplicate)) {
-        duplications += `${id} occurs in the following documents: ${docIndices.join(', ')}\n`
-    }
+	for (const id of ids) {
+		if (unique[id.id]) {
+			if (duplicate[id.id]) duplicate[id.id].push(id.docIndex)
+			else duplicate[id.id] = [unique[id.id], id.docIndex]
+		} else {
+			unique[id.id] = id.docIndex
+		}
+	}
 
-    if (duplications !== "") throw new Error("Duplicate IDs were found:\n" + duplications)
+	const duplicationsMessage = ""
+	for (const [id, docIndices] of Object.entries(duplicate)) {
+		duplicationsMessage += `${id} occurs in the following documents: ${docIndices.join(', ')}\n`
+	}
 
-    
+	if (duplicationsMessage !== "") throw new Error("Duplicate IDs were found:\n" + duplicationsMessage)
+
+	return
 }
 
 const getAllIDsForTagType = (xmlDocs, tagType) => {
-    const allIDs =
-        xmlDocs
-            .map((doc, index) =>
-                [...doc.getElementsByTagName(tagType)].map((element, jndex) => {
-                        const id = getChildByTag(
-                            element,
-                            "ID",
-                            `Document ${index}'s <${tagType}> #${jndex} is missing its <ID>.`
-                        ).innerHTML
+	const allIDs = xmlDocs
+		.map((doc, index) =>                             // for each document,
+			[...doc.getElementsByTagName(tagType)]       // get all <tagType> tags
+			.map((element, jndex) => {                   // for each <tagType>
+				const id = getChildByTag(                // get its child <ID> tag
+					element, 
+					"ID", 
+					`Document ${index}'s <${tagType}> #${jndex} is missing its <ID>.`
+				).innerHTML
+				
+				return {                                 // and return {
+					id,                                  //     id: text between its child <ID> tag and the matching </ID>, 
+					docIndex: index,                     //     docIndex: index of the document,
+					element                              //     element: the <tagType> element (aka the <tagType> element with ID `id`)
+				}                                        // }
+			})
+		)
+		.flat()
 
-                        return {
-                            id,
-                            docIndex: index,
-                            element
-                        }
-                    }
-                )
-            )
-            .flat()
-
-    return allIDs
+    // returns a list of: [
+    //     {
+    //         id: text between its child <ID> tag and the matching </ID>, 
+    //         docIndex: index of the document,
+    //         element: the <tagType> element (aka the <tagType> element with ID `id`)
+    //     }
+    // ]
+	return allIDs
 }
 
 const getElementByIDTag = (idsList, id, errMsg) => {
-    const possibleElements = idsList.filter(idMeta => idMeta.id === id).map(idMeta => idMeta.element)
-    if (possibleElements === null || possibleElements.length === 0) {
-        throw new Error(errMsg);
-    }
+    // ids list is a list returned by getAllIDsForTagType
+    // id is a string
+    // errMsg is also a string
 
-    return possibleElements[0]
+	const possibleElements = idsList.filter(idMeta => idMeta.id === id).map(idMeta => idMeta.element)
+	if (possibleElements === null || possibleElements.length === 0) {
+		throw new Error(errMsg);
+	}
+
+	return possibleElements[0]
 }
 
 const getChildByTag = (xmlElement, tagName, errMsg) => {
-    const possibleChildren = [...xmlElement.children].filter(element => element.tagName === tagName)
-    if (possibleChildren === null || possibleChildren.length === 0) {
-        throw new Error(errMsg);
-    }
+    // gets the first <tagName> child of xmlElement 
 
-    return possibleChildren[0]
+	const possibleChildren = [...xmlElement.children].filter(element => element.tagName === tagName)
+	if (possibleChildren === null || possibleChildren.length === 0) {
+		throw new Error(errMsg);
+	}
+
+	return possibleChildren[0]
 }
 
 
