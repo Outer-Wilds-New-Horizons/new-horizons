cae1a2edbb23a90e5f2cfb975127bbdd9be7ce4c
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-06-06 12:46:20 -0400

    redid all the changes that visual studio undid

--- NewHorizons/Builder/Atmosphere/CloudsBuilder.cs
+++ NewHorizons/Builder/Atmosphere/CloudsBuilder.cs
@@ -10,6 +10,7 @@ namespace NewHorizons.Builder.Atmosphere
     {
         private static Shader _sphereShader = null;
         private static Material[] _gdCloudMaterials;
+        private static Material[] _qmCloudMaterials;
         private static GameObject _lightningPrefab;
         private static Texture2D _colorRamp;
         private static readonly int Color1 = Shader.PropertyToID("_Color");
@@ -19,164 +20,166 @@ namespace NewHorizons.Builder.Atmosphere
         private static readonly int CapTex = Shader.PropertyToID("_CapTex");
         private static readonly int ColorRamp = Shader.PropertyToID("_ColorRamp");
 
-    public static void Make(GameObject planetGO, Sector sector, AtmosphereModule atmo, IModBehaviour mod)
-    {
-        if (_lightningPrefab == null)
-            _lightningPrefab = SearchUtilities.Find("GiantsDeep_Body/Sector_GD/Clouds_GD/LightningGenerator_GD");
-        if (_colorRamp == null)
-            _colorRamp = ImageUtilities.GetTexture(Main.Instance, "AssetBundle/textures/Clouds_Bottom_ramp.png");
-
-        var cloudsMainGO = new GameObject("Clouds");
-        cloudsMainGO.SetActive(false);
-        cloudsMainGO.transform.parent = sector?.transform ?? planetGO.transform;
-
-        MakeTopClouds(cloudsMainGO, atmo, mod);
-
-        var cloudsBottomGO = new GameObject("BottomClouds");
-        cloudsBottomGO.SetActive(false);
-        cloudsBottomGO.transform.parent = cloudsMainGO.transform;
-        cloudsBottomGO.transform.localScale = Vector3.one * atmo.clouds.innerCloudRadius;
-
-        var bottomTSR = cloudsBottomGO.AddComponent<TessellatedSphereRenderer>();
-        bottomTSR.tessellationMeshGroup = SearchUtilities.Find("CloudsBottomLayer_QM")
-            .GetComponent<TessellatedSphereRenderer>().tessellationMeshGroup;
-        var bottomTSRMaterials = SearchUtilities.Find("CloudsBottomLayer_QM").GetComponent<TessellatedSphereRenderer>()
-            .sharedMaterials;
-
-        // If they set a colour apply it to all the materials else keep the default QM one
-        if (atmo.clouds.tint != null)
+        public static void Make(GameObject planetGO, Sector sector, AtmosphereModule atmo, IModBehaviour mod)
         {
-            var bottomColor = atmo.clouds.tint.ToColor();
+            if (_lightningPrefab == null)
+                _lightningPrefab = SearchUtilities.Find("GiantsDeep_Body/Sector_GD/Clouds_GD/LightningGenerator_GD");
+            if (_colorRamp == null)
+                _colorRamp = ImageUtilities.GetTexture(Main.Instance, "AssetBundle/textures/Clouds_Bottom_ramp.png");
 
-            var bottomTSRTempArray = new Material[2];
+            var cloudsMainGO = new GameObject("Clouds");
+            cloudsMainGO.SetActive(false);
+            cloudsMainGO.transform.parent = sector?.transform ?? planetGO.transform;
 
-            bottomTSRTempArray[0] = new Material(bottomTSRMaterials[0]);
-            bottomTSRTempArray[0].SetColor(Color1, bottomColor);
-            bottomTSRTempArray[0].SetColor(TintColor, bottomColor);
-            bottomTSRTempArray[0].SetTexture(ColorRamp, ImageUtilities.TintImage(_colorRamp, bottomColor));
+            MakeTopClouds(cloudsMainGO, atmo, mod);
 
-            bottomTSRTempArray[1] = new Material(bottomTSRMaterials[1]);
+            var cloudsBottomGO = new GameObject("BottomClouds");
+            cloudsBottomGO.SetActive(false);
+            cloudsBottomGO.transform.parent = cloudsMainGO.transform;
+            cloudsBottomGO.transform.localScale = Vector3.one * atmo.clouds.innerCloudRadius;
 
-            bottomTSR.sharedMaterials = bottomTSRTempArray;
-        }
-        else
-        {
-            bottomTSR.sharedMaterials = bottomTSRMaterials;
-        }
+            var bottomTSR = cloudsBottomGO.AddComponent<TessellatedSphereRenderer>();
+            bottomTSR.tessellationMeshGroup = SearchUtilities.Find("CloudsBottomLayer_QM")
+                .GetComponent<TessellatedSphereRenderer>().tessellationMeshGroup;
+            var bottomTSRMaterials = SearchUtilities.Find("CloudsBottomLayer_QM").GetComponent<TessellatedSphereRenderer>()
+                .sharedMaterials;
 
-        bottomTSR.maxLOD = 6;
-        bottomTSR.LODBias = 0;
-        bottomTSR.LODRadius = 1f;
-
-        var bottomTSST = cloudsBottomGO.AddComponent<TessSphereSectorToggle>();
-        bottomTSST._sector = sector;
-
-        var cloudsFluidGO = new GameObject("CloudsFluid");
-        cloudsFluidGO.SetActive(false);
-        cloudsFluidGO.layer = 17;
-        cloudsFluidGO.transform.parent = cloudsMainGO.transform;
-
-        var fluidSC = cloudsFluidGO.AddComponent<SphereCollider>();
-        fluidSC.isTrigger = true;
-        fluidSC.radius = atmo.size;
-
-        var fluidOWSC = cloudsFluidGO.AddComponent<OWShellCollider>();
-        fluidOWSC._innerRadius = atmo.size * 0.9f;
-
-        var fluidCLFV = cloudsFluidGO.AddComponent<CloudLayerFluidVolume>();
-        fluidCLFV._layer = 5;
-        fluidCLFV._priority = 1;
-        fluidCLFV._density = 1.2f;
-
-        var fluidType = FluidVolume.Type.CLOUD;
-
-        try
-        {
-            fluidType = (FluidVolume.Type) Enum.Parse(typeof(FluidVolume.Type),
-                Enum.GetName(typeof(CloudFluidType), atmo.clouds.fluidType).ToUpper());
-        }
-        catch (Exception ex)
-        {
-            Logger.LogError(
-                $"Couldn't parse fluid volume type [{atmo.clouds.fluidType}]: {ex.Message}, {ex.StackTrace}");
-        }
-
-        fluidCLFV._fluidType = fluidType;
-        fluidCLFV._allowShipAutoroll = true;
-        fluidCLFV._disableOnStart = false;
-
-        // Fix the rotations once the rest is done
-        cloudsMainGO.transform.rotation = planetGO.transform.TransformRotation(Quaternion.Euler(0, 0, 0));
-        // For the base shader it has to be rotated idk
-        if (atmo.clouds.cloudsPrefab == CloudPrefabType.Basic)
-            cloudsMainGO.transform.rotation = planetGO.transform.TransformRotation(Quaternion.Euler(90, 0, 0));
-
-        // Lightning
-        if (atmo.clouds.hasLightning)
-        {
-            var lightning = _lightningPrefab.InstantiateInactive();
-            lightning.transform.parent = cloudsMainGO.transform;
-            lightning.transform.localPosition = Vector3.zero;
-
-            var lightningGenerator = lightning.GetComponent<CloudLightningGenerator>();
-            lightningGenerator._altitude = (atmo.clouds.outerCloudRadius + atmo.clouds.innerCloudRadius) / 2f;
-            lightningGenerator._audioSector = sector;
-            if (atmo.clouds.lightningGradient != null)
+            // If they set a colour apply it to all the materials else keep the default QM one
+            if (atmo.clouds.tint != null)
             {
-                var gradient = new GradientColorKey[atmo.clouds.lightningGradient.Length];
+                var bottomColor = atmo.clouds.tint.ToColor();
 
-                for (var i = 0; i < atmo.clouds.lightningGradient.Length; i++)
-                {
-                    var pair = atmo.clouds.lightningGradient[i];
-                    gradient[i] = new GradientColorKey(pair.tint.ToColor(), pair.time);
-                }
+                var bottomTSRTempArray = new Material[2];
 
-                lightningGenerator._lightColor.colorKeys = gradient;
+                bottomTSRTempArray[0] = new Material(bottomTSRMaterials[0]);
+                bottomTSRTempArray[0].SetColor(Color1, bottomColor);
+                bottomTSRTempArray[0].SetColor(TintColor, bottomColor);
+                bottomTSRTempArray[0].SetTexture(ColorRamp, ImageUtilities.TintImage(_colorRamp, bottomColor));
+
+                bottomTSRTempArray[1] = new Material(bottomTSRMaterials[1]);
+
+                bottomTSR.sharedMaterials = bottomTSRTempArray;
+            }
+            else
+            {
+                bottomTSR.sharedMaterials = bottomTSRMaterials;
             }
 
-            lightning.SetActive(true);
+            bottomTSR.maxLOD = 6;
+            bottomTSR.LODBias = 0;
+            bottomTSR.LODRadius = 1f;
+
+            var bottomTSST = cloudsBottomGO.AddComponent<TessSphereSectorToggle>();
+            bottomTSST._sector = sector;
+
+            var cloudsFluidGO = new GameObject("CloudsFluid");
+            cloudsFluidGO.SetActive(false);
+            cloudsFluidGO.layer = 17;
+            cloudsFluidGO.transform.parent = cloudsMainGO.transform;
+
+            var fluidSC = cloudsFluidGO.AddComponent<SphereCollider>();
+            fluidSC.isTrigger = true;
+            fluidSC.radius = atmo.size;
+
+            var fluidOWSC = cloudsFluidGO.AddComponent<OWShellCollider>();
+            fluidOWSC._innerRadius = atmo.size * 0.9f;
+
+            var fluidCLFV = cloudsFluidGO.AddComponent<CloudLayerFluidVolume>();
+            fluidCLFV._layer = 5;
+            fluidCLFV._priority = 1;
+            fluidCLFV._density = 1.2f;
+
+            var fluidType = FluidVolume.Type.CLOUD;
+
+            try
+            {
+                fluidType = (FluidVolume.Type) Enum.Parse(typeof(FluidVolume.Type),
+                    Enum.GetName(typeof(CloudFluidType), atmo.clouds.fluidType).ToUpper());
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(
+                    $"Couldn't parse fluid volume type [{atmo.clouds.fluidType}]: {ex.Message}, {ex.StackTrace}");
+            }
+
+            fluidCLFV._fluidType = fluidType;
+            fluidCLFV._allowShipAutoroll = true;
+            fluidCLFV._disableOnStart = false;
+
+            // Fix the rotations once the rest is done
+            cloudsMainGO.transform.rotation = planetGO.transform.TransformRotation(Quaternion.Euler(0, 0, 0));
+            // For the base shader it has to be rotated idk
+            if (atmo.clouds.cloudsPrefab == CloudPrefabType.Basic)
+                cloudsMainGO.transform.rotation = planetGO.transform.TransformRotation(Quaternion.Euler(90, 0, 0));
+
+            // Lightning
+            if (atmo.clouds.hasLightning)
+            {
+                var lightning = _lightningPrefab.InstantiateInactive();
+                lightning.transform.parent = cloudsMainGO.transform;
+                lightning.transform.localPosition = Vector3.zero;
+
+                var lightningGenerator = lightning.GetComponent<CloudLightningGenerator>();
+                lightningGenerator._altitude = (atmo.clouds.outerCloudRadius + atmo.clouds.innerCloudRadius) / 2f;
+                lightningGenerator._audioSector = sector;
+                if (atmo.clouds.lightningGradient != null)
+                {
+                    var gradient = new GradientColorKey[atmo.clouds.lightningGradient.Length];
+
+                    for (var i = 0; i < atmo.clouds.lightningGradient.Length; i++)
+                    {
+                        var pair = atmo.clouds.lightningGradient[i];
+                        gradient[i] = new GradientColorKey(pair.tint.ToColor(), pair.time);
+                    }
+
+                    lightningGenerator._lightColor.colorKeys = gradient;
+                }
+
+                lightning.SetActive(true);
+            }
+
+            cloudsMainGO.transform.position = planetGO.transform.TransformPoint(Vector3.zero);
+            cloudsBottomGO.transform.position = planetGO.transform.TransformPoint(Vector3.zero);
+            cloudsFluidGO.transform.position = planetGO.transform.TransformPoint(Vector3.zero);
+
+            cloudsBottomGO.SetActive(true);
+            cloudsFluidGO.SetActive(true);
+            cloudsMainGO.SetActive(true);
         }
 
-        cloudsMainGO.transform.position = planetGO.transform.TransformPoint(Vector3.zero);
-        cloudsBottomGO.transform.position = planetGO.transform.TransformPoint(Vector3.zero);
-        cloudsFluidGO.transform.position = planetGO.transform.TransformPoint(Vector3.zero);
-
-        cloudsBottomGO.SetActive(true);
-        cloudsFluidGO.SetActive(true);
-        cloudsMainGO.SetActive(true);
-    }
-
-    public static GameObject MakeTopClouds(GameObject rootObject, AtmosphereModule atmo, IModBehaviour mod)
-    {
-        var cloudTint = atmo.clouds.tint?.ToColor() ?? Color.white;
-
-        Texture2D image, cap, ramp;
-
-        try
+        public static GameObject MakeTopClouds(GameObject rootObject, AtmosphereModule atmo, IModBehaviour mod)
         {
-            image = ImageUtilities.GetTexture(mod, atmo.clouds.texturePath);
+            var cloudTint = atmo.clouds.tint?.ToColor() ?? Color.white;
 
-            cap = atmo.clouds.capPath == null ? ImageUtilities.ClearTexture(128, 128) : ImageUtilities.GetTexture(mod, atmo.clouds.capPath);
-            ramp = atmo.clouds.rampPath == null ? ImageUtilities.CanvasScaled(image, 1, image.height) : ImageUtilities.GetTexture(mod, atmo.clouds.rampPath);
-        }
-        catch (Exception e)
-        {
-            Logger.LogError($"Couldn't load Cloud textures for [{rootObject.name}], {e.Message}, {e.StackTrace}");
-            return null;
-        }
+            Texture2D image, cap, ramp;
 
-        var cloudsTopGO = new GameObject("TopClouds");
-        cloudsTopGO.SetActive(false);
-        cloudsTopGO.transform.parent = rootObject.transform;
-        cloudsTopGO.transform.localScale = Vector3.one * atmo.clouds.outerCloudRadius;
+            try
+            {
+                image = ImageUtilities.GetTexture(mod, atmo.clouds.texturePath);
 
-        var topMF = cloudsTopGO.AddComponent<MeshFilter>();
-        topMF.mesh = SearchUtilities.Find("CloudsTopLayer_GD").GetComponent<MeshFilter>().mesh;
+                cap = atmo.clouds.capPath == null ? ImageUtilities.ClearTexture(128, 128) : ImageUtilities.GetTexture(mod, atmo.clouds.capPath);
+                ramp = atmo.clouds.rampPath == null ? ImageUtilities.CanvasScaled(image, 1, image.height) : ImageUtilities.GetTexture(mod, atmo.clouds.rampPath);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"Couldn't load Cloud textures for [{rootObject.name}], {e.Message}, {e.StackTrace}");
+                return null;
+            }
 
-        var topMR = cloudsTopGO.AddComponent<MeshRenderer>();
+            var cloudsTopGO = new GameObject("TopClouds");
+            cloudsTopGO.SetActive(false);
+            cloudsTopGO.transform.parent = rootObject.transform;
+            cloudsTopGO.transform.localScale = Vector3.one * atmo.clouds.outerCloudRadius;
+
+            var topMF = cloudsTopGO.AddComponent<MeshFilter>();
+            topMF.mesh = SearchUtilities.Find("CloudsTopLayer_GD").GetComponent<MeshFilter>().mesh;
+
+            var topMR = cloudsTopGO.AddComponent<MeshRenderer>();
 
             if (_sphereShader == null) _sphereShader = Main.NHAssetBundle.LoadAsset<Shader>("Assets/Shaders/SphereTextureWrapper.shader");
             if (_gdCloudMaterials == null) _gdCloudMaterials = SearchUtilities.Find("CloudsTopLayer_GD").GetComponent<MeshRenderer>().sharedMaterials;
+            if (_qmCloudMaterials == null) _qmCloudMaterials = SearchUtilities.Find("CloudsTopLayer_QM").GetComponent<MeshRenderer>().sharedMaterials;
+            Material[] prefabMaterials = atmo.clouds.cloudsPrefab == CloudPrefabType.GiantsDeep ? _gdCloudMaterials : _qmCloudMaterials;
             var tempArray = new Material[2];
 
             if (atmo.clouds.cloudsPrefab == CloudPrefabType.Basic)
@@ -189,14 +192,14 @@ namespace NewHorizons.Builder.Atmosphere
             }
             else
             {
-                var material = new Material(_gdCloudMaterials[0]);
+                var material = new Material(prefabMaterials[0]);
                 if (atmo.clouds.unlit) material.renderQueue = 2550;
                 material.name = atmo.clouds.unlit ? "AdvancedCloud" : "AdvancedShadowCloud";
                 tempArray[0] = material;
             }
 
             // This is the stencil material for the fog under the clouds
-            tempArray[1] = new Material(_gdCloudMaterials[1]);
+            tempArray[1] = new Material(prefabMaterials[1]);
             topMR.sharedMaterials = tempArray;
 
             foreach (var material in topMR.sharedMaterials)
