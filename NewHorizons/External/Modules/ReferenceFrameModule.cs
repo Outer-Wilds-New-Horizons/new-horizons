c0ad3a0fa74708a24e653fe390483e2f9598db2e
Nick <nicholasconnors@hotmail.ca>
2022-06-15 14:37:25 -0400

    Made maxTargetDistance accurate and added targetColliderRadius

--- NewHorizons/External/Modules/ReferenceFrameModule.cs
+++ NewHorizons/External/Modules/ReferenceFrameModule.cs
@@ -16,7 +16,7 @@ namespace NewHorizons.External.Modules
         public bool hideInMap;
 
         /// <summary>
-        /// Radius of the brackets that show up when you target this. Defaults to the sphereOfInfluence.
+        /// Radius of the brackets that show up when you target this. Defaults to the sphere of influence.
         /// </summary>
         [DefaultValue(-1)] public float bracketRadius = -1;
 
@@ -26,8 +26,13 @@ namespace NewHorizons.External.Modules
         public bool targetWhenClose;
 
         /// <summary>
-        /// The maximum distance that the reference frame can be targeted from. Defaults to double the sphereOfInfluence.
+        /// The maximum distance that the reference frame can be targeted from. Defaults to 100km and cannot be greater than that.
         /// </summary>
-        [DefaultValue(-1)] public float maxTargetDistance = -1;
+        public float maxTargetDistance; // If it's less than or equal to zero the game makes it 100km 
+
+        /// <summary>
+        /// The radius of the sphere around the planet which you can click on to target it. Defaults to twice the sphere of influence.
+        /// </summary>
+        public float targetColliderRadius;
     }
 }
