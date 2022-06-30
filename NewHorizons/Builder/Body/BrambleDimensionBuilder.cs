dbc36bbfc7e37b42dee8d2a80d1595cbeb9cadcd
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-06-29 22:04:16 -0400

    dimensions now are placed so they don't overlap with anything

--- NewHorizons/Builder/Body/BrambleDimensionBuilder.cs
+++ NewHorizons/Builder/Body/BrambleDimensionBuilder.cs
@@ -11,6 +11,21 @@ namespace NewHorizons.Builder.Body
 {
     public static class BrambleDimensionBuilder
     {
+        // put node here
+        //  "position": {"x": 65.2428, "y": -137.305, "z": 198.1078}, "normal": {"x": 0.303696, "y": -0.5609235, "z": 0.7701519}
+
+        // location of all vanilla bramble dimensions
+        //-9116.795 -19873.44 2480.327
+        //-8460.688 -19873.44 6706.444
+        //-5015.165 -19873.44 4142.816
+        //-8993.414 -17059.44 4521.747
+        //-7044.813 -17135.44 3272.149
+        //-6904.48  -17048.44 5574.479
+        //-11096.95 -22786.44 4657.534
+        //-8716.807 -22786.44 4496.394
+
+        private static int DIMENSION_COUNTER = 0;
+
         public static GameObject Make(NewHorizonsBody body)
         {
             var config = body.Config.Bramble.dimension;
@@ -25,6 +40,10 @@ namespace NewHorizons.Builder.Body
             ao._customName = name;
             ao._name = AstroObject.Name.CustomString;
             dimension.name = name.Replace(" ", "").Replace("'", "") + "_Body";
+
+            // set position
+            ao.transform.position = new Vector3(6904.48f, 17048.44f, 5574.479f) + new Vector3(0, 3000, 0)*DIMENSION_COUNTER;
+            DIMENSION_COUNTER++;
 
             // TODO: radius (need to determine what the base radius is first)
 
