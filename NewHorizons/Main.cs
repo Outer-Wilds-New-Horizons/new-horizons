44f6dfa0705af64a84e50a6aa2d89733590b1e02
Will Corby <williamforgames6@gmail.com>
2022-07-05 15:24:22 -0700

    do da hokey pokey
    make it so verbose logs can still happen even if debug mode is off

--- NewHorizons/Main.cs
+++ NewHorizons/Main.cs
@@ -89,7 +89,7 @@ namespace NewHorizons
                 DebugMenu.UpdatePauseMenuButton();
             }
 
-            if (Debug && VerboseLogs) Logger.UpdateLogLevel(Logger.LogType.Verbose);
+            if (VerboseLogs)          Logger.UpdateLogLevel(Logger.LogType.Verbose);
             else if (Debug)           Logger.UpdateLogLevel(Logger.LogType.Log);
             else                      Logger.UpdateLogLevel(Logger.LogType.Error);
 
