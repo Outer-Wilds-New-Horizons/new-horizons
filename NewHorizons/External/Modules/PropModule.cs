397d9ec4c86dd62165f16e224f4750ac96a62c05
Noah Pilarski <mehmehmeh313@gmail.com>
2022-07-18 17:02:01 -0400

    Don't use game's enum

--- NewHorizons/External/Modules/PropModule.cs
+++ NewHorizons/External/Modules/PropModule.cs
@@ -745,7 +745,28 @@ namespace NewHorizons.External.Modules
             /// <summary>
             /// The audio track of this audio volume
             /// </summary>
-            public OWAudioMixer.TrackName track = OWAudioMixer.TrackName.Environment;
+            public AudioMixerTrackName track = AudioMixerTrackName.Environment;
         }
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum AudioMixerTrackName
+    {
+        [EnumMember(Value = @"undefined")] Undefined = 0,
+        [EnumMember(Value = @"menu")] Menu = 1,
+        [EnumMember(Value = @"music")] Music = 2,
+        [EnumMember(Value = @"environment")] Environment = 4,
+        [EnumMember(Value = @"environmentUnfiltered")] Environment_Unfiltered = 5,
+        [EnumMember(Value = @"endTimesSfx")] EndTimes_SFX = 8,
+        [EnumMember(Value = @"signal")] Signal = 16,
+        [EnumMember(Value = @"death")] Death = 32,
+        [EnumMember(Value = @"player")] Player = 64,
+        [EnumMember(Value = @"playerExternal")] Player_External = 65,
+        [EnumMember(Value = @"ship")] Ship = 128,
+        [EnumMember(Value = @"map")] Map = 256,
+        [EnumMember(Value = @"endTimesMusic")] EndTimes_Music = 512,
+        [EnumMember(Value = @"muffleWhileRafting")] MuffleWhileRafting = 1024,
+        [EnumMember(Value = @"muffleIndoors")] MuffleIndoors = 2048,
+        [EnumMember(Value = @"slideReelMusic")] SlideReelMusic = 4096,
     }
 }
