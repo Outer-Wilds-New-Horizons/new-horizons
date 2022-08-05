2b6bb6c5d25e7c90b9890a4d1e7bf028f2ece110
Nick <nicholasconnors@hotmail.ca>
2022-08-05 19:57:14 -0400

    Probably less laggy

--- NewHorizons/Handlers/StreamingHandler.cs
+++ NewHorizons/Handlers/StreamingHandler.cs
@@ -83,8 +83,10 @@ namespace NewHorizons.Handlers
             {
                 sector.OnOccupantEnterSector += _ =>
                 {
-                    foreach (var assetBundle in assetBundles)
-                        StreamingManager.LoadStreamingAssets(assetBundle);
+                    // If both are already in then it already loaded
+                    if (sector.ContainsOccupant(DynamicOccupant.Player) && sector.ContainsOccupant(DynamicOccupant.Probe)) return;
+
+                    foreach (var assetBundle in assetBundles) StreamingManager.LoadStreamingAssets(assetBundle);
                 };
                 /*
                 sector.OnOccupantExitSector += _ =>
