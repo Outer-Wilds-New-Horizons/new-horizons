aeea50293aa249cb600db56a7601a1554f1a7047
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-11 16:05:30 -0400

    Use ambient light already in eye scene

--- NewHorizons/Builder/General/AmbientLightBuilder.cs
+++ NewHorizons/Builder/General/AmbientLightBuilder.cs
@@ -6,15 +6,14 @@ namespace NewHorizons.Builder.General
     {
         public static void Make(GameObject planetGO, Sector sector, float scale, float intensity)
         {
-            var bhAmbientLight = SearchUtilities.Find("BrittleHollow_Body/AmbientLight_BH_Surface");
-            if (bhAmbientLight == null) return;
+            var ambientLight = Main.Instance.CurrentStarSystem == "EyeOfTheUniverse" ? SearchUtilities.Find("EyeOfTheUniverse_Body/Sector_EyeOfTheUniverse/SixthPlanet_Root/QuantumMoonProxy_Pivot/QuantumMoonProxy_Root/MoonState_Root/AmbientLight_QM") : SearchUtilities.Find("BrittleHollow_Body/AmbientLight_BH_Surface");
+            if (ambientLight == null) return;
 
-            GameObject lightGO = GameObject.Instantiate(bhAmbientLight, sector?.transform ?? planetGO.transform);
+            GameObject lightGO = GameObject.Instantiate(ambientLight, sector?.transform ?? planetGO.transform);
             lightGO.transform.position = planetGO.transform.position;
-            lightGO.name = "Light";
+            lightGO.name = "AmbientLight";
 
             var light = lightGO.GetComponent<Light>();
-            light.name = "AmbientLight";
             /* R is related to the inner radius of the ambient light volume
              * G is if its a shell or not. 1.0f for shell else 0.0f.
              * B is just 1.0 always I think, altho no because changing it changes the brightness so idk
