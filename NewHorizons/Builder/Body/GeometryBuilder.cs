9532bd1206ae619ace875869b21077ac3cc998e8
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-11 14:50:50 -0400

    Fix NRE in GeometryBuilder.cs

--- NewHorizons/Builder/Body/GeometryBuilder.cs
+++ NewHorizons/Builder/Body/GeometryBuilder.cs
@@ -12,7 +12,8 @@ namespace NewHorizons.Builder.Body
             groundGO.transform.parent = sector?.transform ?? planetGO.transform;
             groundGO.transform.localScale = new Vector3(groundScale, groundScale, groundScale);
             groundGO.transform.position = planetGO.transform.position;
-            groundGO.GetComponent<MeshFilter>().mesh = SearchUtilities.Find("CloudsTopLayer_GD").GetComponent<MeshFilter>().mesh;
+            var topLayer = SearchUtilities.Find("CloudsTopLayer_GD");
+            if (topLayer != null) groundGO.GetComponent<MeshFilter>().mesh = topLayer.GetComponent<MeshFilter>().mesh;
             groundGO.GetComponent<SphereCollider>().radius = 1f;
             groundGO.SetActive(true);
         }
