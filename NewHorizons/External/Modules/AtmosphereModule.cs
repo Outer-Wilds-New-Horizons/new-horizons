f1b311f246475ef32cb86bf4ada6628c5ebb3498
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-14 16:53:31 -0400

    clarify

--- NewHorizons/External/Modules/AtmosphereModule.cs
+++ NewHorizons/External/Modules/AtmosphereModule.cs
@@ -165,7 +165,7 @@ namespace NewHorizons.External.Modules
             public bool unlit;
 
             /// <summary>
-            /// How fast the clouds will rotate in degrees per second.
+            /// How fast the clouds will rotate relative to the planet in degrees per second.
             /// </summary>
             [DefaultValue(0f)] public float rotationSpeed = 0f;
 
