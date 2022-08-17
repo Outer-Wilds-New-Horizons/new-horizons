3cd85bfcd36ae4aa58f3852167cd620011195d09
JohnCorby <williamforgames6@gmail.com>
2022-08-17 11:42:35 -0700

    do it properly now that we're up to date with dev

--- NewHorizons/Main.cs
+++ NewHorizons/Main.cs
@@ -470,7 +470,7 @@ namespace NewHorizons
                         Logger.LogVerbose($"Loading system {name}");
 
                         var relativePath = file.Replace(folder, "");
-                        var starSystemConfig = mod.ModHelper.Storage.Load<StarSystemConfig>(relativePath);
+                        var starSystemConfig = mod.ModHelper.Storage.Load<StarSystemConfig>(relativePath, false);
                         starSystemConfig.Migrate();
                         starSystemConfig.FixCoordinates();
 
@@ -535,7 +535,7 @@ namespace NewHorizons
         {
             Logger.LogVerbose($"Loading addon manifest for {mod.ModHelper.Manifest.Name}");
 
-            var addonConfig = mod.ModHelper.Storage.Load<AddonConfig>(file);
+            var addonConfig = mod.ModHelper.Storage.Load<AddonConfig>(file, false);
 
             if (addonConfig.achievements != null) AchievementHandler.RegisterAddon(addonConfig, mod as ModBehaviour);
             if (addonConfig.credits != null) CreditsHandler.RegisterCredits(mod.ModHelper.Manifest.Name, addonConfig.credits);
@@ -574,7 +574,7 @@ namespace NewHorizons
             NewHorizonsBody body = null;
             try
             {
-                var config = mod.ModHelper.Storage.Load<PlanetConfig>(relativePath);
+                var config = mod.ModHelper.Storage.Load<PlanetConfig>(relativePath, false);
                 if (config == null)
                 {
                     Logger.LogError($"Couldn't load {relativePath}. Is your Json formatted correctly?");
@@ -586,7 +586,7 @@ namespace NewHorizons
                 if (!SystemDict.ContainsKey(config.starSystem))
                 {
                     // Since we didn't load it earlier there shouldn't be a star system config
-                    var starSystemConfig = mod.ModHelper.Storage.Load<StarSystemConfig>($"systems/{config.starSystem}.json");
+                    var starSystemConfig = mod.ModHelper.Storage.Load<StarSystemConfig>($"systems/{config.starSystem}.json", false);
                     if (starSystemConfig == null) starSystemConfig = new StarSystemConfig();
                     else Logger.LogWarning($"Loaded system config for {config.starSystem}. Why wasn't this loaded earlier?");
 
