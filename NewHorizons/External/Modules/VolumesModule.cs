9ad1612c0f0b9db2759f68acbe7963a5289ae4ed
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-31 16:40:05 -0400

    .

--- NewHorizons/External/Modules/VolumesModule.cs
+++ NewHorizons/External/Modules/VolumesModule.cs
@@ -15,37 +15,37 @@ namespace NewHorizons.External.Modules
     public class VolumesModule
     {
         /// <summary>
-        /// Add audio volumes to this planet
+        /// Add audio volumes to this planet.
         /// </summary>
         public AudioVolumeInfo[] audioVolumes;
 
         /// <summary>
-        /// Add hazard volumes to this planet
+        /// Add hazard volumes to this planet.
         /// </summary>
         public HazardVolumeInfo[] hazardVolumes;
 
         /// <summary>
-        /// Add interference volumes to this planet
+        /// Add interference volumes to this planet.
         /// </summary>
         public VolumeInfo[] interferenceVolumes;
 
         /// <summary>
-        /// Add insulating volumes to this planet
+        /// Add insulating volumes to this planet. These will stop electricty hazard volumes from affecting you (just like the jellyfish).
         /// </summary>
         public VolumeInfo[] insulatingVolumes;
 
         /// <summary>
-        /// Add map restriction volumes to this planet
+        /// Add map restriction volumes to this planet.
         /// </summary>
         public VolumeInfo[] mapRestrictionVolumes;
 
         /// <summary>
-        /// Add notification volumes to this planet
+        /// Add notification volumes to this planet.
         /// </summary>
         public NotificationVolumeInfo[] notificationVolumes;
 
         /// <summary>
-        /// Add triggers that reveal parts of the ship log on this planet
+        /// Add triggers that reveal parts of the ship log on this planet.
         /// </summary>
         public RevealVolumeInfo[] revealVolumes;
 
