bf6745d351216dfb7ea6d3bbace9de45ba01277c
JohnCorby <williamforgames6@gmail.com>
2022-09-07 17:21:42 -0700

    groups on details

---
+++ NewHorizons/Builder/General/GroupBuilder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Logger = NewHorizons.Utility.Logger;
+
+namespace NewHorizons.Builder.General;
+
+public static class GroupBuilder
+{
+    /// <summary>
+    /// puts groups on objects.
+    /// run this before the gameobject is active.
+    /// </summary>
+    public static void Make(GameObject go, Sector sector)
+    {
+        Logger.LogVerbose($"putting groups on {go} (linked to {sector})");
+        if (!sector)
+        {
+            Logger.LogWarning("tried to put groups on a null sector");
+            return;
+        }
+        if (go.activeInHierarchy)
+        {
+            Logger.LogWarning("tried to put groups on an active gameobject");
+            return;
+        }
+
+        go.GetAddComponent<SectorCullGroup>()._sector = sector;
+        go.GetAddComponent<SectorCollisionGroup>()._sector = sector;
+        go.GetAddComponent<SectorLightsCullGroup>()._sector = sector;
+    }
+}
