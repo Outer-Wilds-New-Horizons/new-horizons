44cf3d3fdcf5e150e563057afb064c789e3780c9
Nick <nicholasconnors@hotmail.ca>
2022-09-10 10:07:26 -0400

    Remove parentheses

--- NewHorizons/Patches/CharacterDialogueTreePatches.cs
+++ NewHorizons/Patches/CharacterDialogueTreePatches.cs
@@ -9,13 +9,13 @@ internal class CharacterDialogueTreePatches
     [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.Awake))]
     private static void CharacterDialogueTree_Awake(CharacterDialogueTree __instance)
     {
-        GlobalMessenger<OWRigidbody>.AddListener("AttachPlayerToPoint", (_) => __instance.EndConversation());
+        GlobalMessenger<OWRigidbody>.AddListener("AttachPlayerToPoint", _ => __instance.EndConversation());
     }
 
     [HarmonyPrefix]
     [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.OnDestroy))]
     private static void CharacterDialogueTree_OnDestroy(CharacterDialogueTree __instance)
     {
-        GlobalMessenger<OWRigidbody>.RemoveListener("AttachPlayerToPoint", (_) => __instance.EndConversation());
+        GlobalMessenger<OWRigidbody>.RemoveListener("AttachPlayerToPoint", _ => __instance.EndConversation());
     }
 }
