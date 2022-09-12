ab237624b22df50f2d3ae01c3e6e047a94f099b4
Noah Pilarski <mehmehmeh313@gmail.com>
2022-09-12 19:44:54 -0400

    Fix cloak

--- NewHorizons/Patches/CloakPatches.cs
+++ NewHorizons/Patches/CloakPatches.cs
@@ -1,10 +1,8 @@
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace NewHorizons.Patches
 {
+    [HarmonyPatch]
     public static class CloakPatches
     {
         [HarmonyPrefix]
@@ -16,21 +14,21 @@ namespace NewHorizons.Patches
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(CloakFieldController), nameof(CloakFieldController.isPlayerInsideCloak), MethodType.Getter)]
-        public static void CloakFieldController_isPlayerInsideCloak(CloakFieldController __instance, ref bool __result)
+        public static void CloakFieldController_isPlayerInsideCloak(ref bool __result)
         {
             __result = __result || Components.CloakSectorController.isPlayerInside;
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(CloakFieldController), nameof(CloakFieldController.isProbeInsideCloak), MethodType.Getter)]
-        public static void CloakFieldController_isProbeInsideCloak(CloakFieldController __instance, ref bool __result)
+        public static void CloakFieldController_isProbeInsideCloak(ref bool __result)
         {
             __result = __result || Components.CloakSectorController.isProbeInside;
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(CloakFieldController), nameof(CloakFieldController.isShipInsideCloak), MethodType.Getter)]
-        public static void CloakFieldController_isShipInsideCloak(CloakFieldController __instance, ref bool __result)
+        public static void CloakFieldController_isShipInsideCloak(ref bool __result)
         {
             __result = __result || Components.CloakSectorController.isShipInside;
         }
