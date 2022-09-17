223b35c06626efb36c4aa495189163a3b661cd57
Noah Pilarski <mehmehmeh313@gmail.com>
2022-09-17 12:05:20 -0400

    return zero for null MVector3

--- NewHorizons/Utility/MVector3.cs
+++ NewHorizons/Utility/MVector3.cs
@@ -23,6 +23,7 @@ namespace NewHorizons.Utility
 
         public static implicit operator Vector3(MVector3 vec)
         {
+            if (vec == null) return Vector3.zero;
             return new Vector3(vec.x, vec.y, vec.z);
         }
     }
