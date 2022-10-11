58ee06fb7b4401134d2e615d5bad689a7e5dfe86
Noah Pilarski <mehmehmeh313@gmail.com>
2022-10-10 23:41:06 -0400

    Add 2 comments

--- NewHorizons/Patches/MeteorPatches.cs
+++ NewHorizons/Patches/MeteorPatches.cs
@@ -9,6 +9,7 @@ namespace NewHorizons.Patches
         [HarmonyPatch(typeof(MeteorController), nameof(MeteorController.Suspend), new System.Type[0])]
         public static void MeteorController_Suspend(MeteorController __instance)
         {
+            // Meteors launch inactive because of prefab. So let's fix that.
             __instance.gameObject.SetActive(true);
         }
     }
