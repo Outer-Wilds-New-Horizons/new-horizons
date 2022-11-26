16131fa6f451dece613bf7daa3c216964a80cad0
Noah Pilarski <mehmehmeh313@gmail.com>
2022-11-26 15:10:16 -0500

    Earn probe lost achievement with destruction volumes too
    + only set entered eye condition when at the eye

--- NewHorizons/Patches/AchievementPatches.cs
+++ NewHorizons/Patches/AchievementPatches.cs
@@ -1,6 +1,7 @@
 using HarmonyLib;
 using NewHorizons.OtherMods.AchievementsPlus.NH;
 using System.Linq;
+using UnityEngine;
 
 namespace NewHorizons.Patches
 {
@@ -15,6 +16,27 @@ namespace NewHorizons.Patches
             {
                 SuckedIntoLavaByTornadoAchievement.Earn();
             }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ProbeDestructionDetector), nameof(ProbeDestructionDetector.FixedUpdate))]
+        public static bool ProbeDestructionDetector_FixedUpdate(ProbeDestructionDetector __instance)
+        {
+            if (__instance._activeVolumes.Count > 0 && __instance._safetyVolumes.Count == 0)
+            {
+                if (LoadManager.GetCurrentScene() == OWScene.EyeOfTheUniverse)
+                {
+                    DialogueConditionManager.SharedInstance.SetConditionState("PROBE_ENTERED_EYE", conditionState: true);
+                    Debug.Log("PROBE DESTROYED (ENTERED THE EYE)");
+                }
+                else
+                    Debug.Log("PROBE DESTROYED");
+
+                ProbeLostAchievement.Earn();
+                Object.Destroy(__instance._probe.gameObject);
+            }
+            __instance.enabled = false;
+            return false;
         }
     }
 }
