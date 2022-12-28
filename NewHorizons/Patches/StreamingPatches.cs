f8de674a6d7c30769c10fafcdb09705f0e0b7bb2
JohnCorby <williamforgames6@gmail.com>
2022-12-28 12:06:49 -0800

    filter out goofy error

--- NewHorizons/Patches/StreamingPatches.cs
+++ NewHorizons/Patches/StreamingPatches.cs
@@ -1,10 +1,11 @@
 using HarmonyLib;
 using NewHorizons.Handlers;
+using OWML.Logging;
 
 namespace NewHorizons.Patches
 {
     [HarmonyPatch]
-    public static class StreamingManagerPatches
+    public static class StreamingPatches
     {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(StreamingManager), nameof(StreamingManager.UnloadStreamingAssets))]
@@ -12,6 +13,14 @@ namespace NewHorizons.Patches
         {
             // Only let it unload stuff that isn't being used
             return !StreamingHandler.IsBundleInUse(assetBundleName);
-        } 
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UnityLogger), "OnLogMessageReceived")]
+        public static bool UnityLogger_OnLogMessageReceived(string message)
+        {
+            // Filter out goofy error that doesn't actually break anything
+            return !message.Contains(" is out of bounds (size=0)");
+        }
     }
 }
