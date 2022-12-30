b927366410e6370ba2beac006783e07a4913aaca
Nick <nicholasconnors@hotmail.ca>
2022-12-30 16:31:23 -0500

    Cache _tables also try tracking stock assets

--- NewHorizons/Handlers/StreamingHandler.cs
+++ NewHorizons/Handlers/StreamingHandler.cs
@@ -10,14 +10,34 @@ namespace NewHorizons.Handlers
     public static class StreamingHandler
     {
         private static readonly Dictionary<Material, string> _materialCache = new();
-        private static readonly Dictionary<GameObject, string[]> _objectCache = new();
+        private static readonly Dictionary<GameObject, List<string>> _objectCache = new();
         private static readonly Dictionary<string, List<Sector>> _sectorCache = new();
+        private static StreamingMaterialTable[] _tables;
 
         public static void Init()
         {
             _materialCache.Clear();
             _objectCache.Clear();
             _sectorCache.Clear();
+
+            _tables = Resources.FindObjectsOfTypeAll<StreamingMaterialTable>();
+
+            // Track base game assets
+            foreach (var streamingGroup in GameObject.FindObjectsOfType<StreamingGroup>())
+            {
+                var sector = streamingGroup.GetAttachedOWRigidbody().GetComponentInChildren<Sector>();
+                var assetBundles = streamingGroup._streamingMaterialTables.Select(x => x.assetBundle);
+
+                foreach (var assetBundle in assetBundles)
+                {
+                    if (!_sectorCache.TryGetValue(assetBundle, out var sectors))
+                    {
+                        sectors = new List<Sector>();
+                        _sectorCache.Add(assetBundle, sectors);
+                    }
+                    sectors.SafeAdd(sector);
+                }
+            }
         }
 
         public static void SetUpStreaming(AstroObject.Name name, Sector sector)
@@ -52,13 +72,12 @@ namespace NewHorizons.Handlers
             // tries the cache first, then builds
             if (!_objectCache.TryGetValue(obj, out var assetBundles))
             {
-                var assetBundlesList = new List<string>();
+                assetBundles = new List<string>();
 
-                var tables = Resources.FindObjectsOfTypeAll<StreamingMaterialTable>();
                 foreach (var streamingHandle in obj.GetComponentsInChildren<StreamingMeshHandle>())
                 {
                     var assetBundle = streamingHandle.assetBundle;
-                    assetBundlesList.SafeAdd(assetBundle);
+                    assetBundles.SafeAdd(assetBundle);
 
                     if (streamingHandle is StreamingRenderMeshHandle or StreamingSkinnedMeshHandle)
                     {
@@ -69,18 +88,18 @@ namespace NewHorizons.Handlers
                         // Gonna assume that if theres more than one material its probably in the same asset bundle anyway right
                         if (_materialCache.TryGetValue(materials[0], out assetBundle))
                         {
-                            assetBundlesList.SafeAdd(assetBundle);
+                            assetBundles.SafeAdd(assetBundle);
                         }
                         else
                         {
-                            foreach (var table in tables)
+                            foreach (var table in _tables)
                             {
                                 foreach (var lookup in table._materialPropertyLookups)
                                 {
                                     if (materials.Contains(lookup.material))
                                     {
                                         _materialCache.SafeAdd(lookup.material, table.assetBundle);
-                                        assetBundlesList.SafeAdd(table.assetBundle);
+                                        assetBundles.SafeAdd(table.assetBundle);
                                     }
                                 }
                             }
@@ -88,7 +107,6 @@ namespace NewHorizons.Handlers
                     }
                 }
 
-                assetBundles = assetBundlesList.ToArray();
                 _objectCache[obj] = assetBundles;
             }
 
