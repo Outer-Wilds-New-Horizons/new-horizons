ae960ff2088723ace20e235fdcfb77080907b329
JohnCorby <williamforgames6@gmail.com>
2022-12-31 15:02:31 -0800

    why did it add braces

--- NewHorizons/Handlers/StreamingHandler.cs
+++ NewHorizons/Handlers/StreamingHandler.cs
@@ -152,9 +152,7 @@ namespace NewHorizons.Handlers
                 {
                     if (!sector.ContainsAnyOccupants(DynamicOccupant.Player | DynamicOccupant.Probe))
                         foreach (var assetBundle in assetBundles)
-                        {
                             StreamingManager.UnloadStreamingAssets(assetBundle, 5f);
-                        }
                 };
             }
             else
