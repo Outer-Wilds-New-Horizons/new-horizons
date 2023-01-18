362204dcbbed390b6916e50337de8e167af32dfd
Nick <nicholasconnors@hotmail.ca>
2023-01-17 23:52:16 -0500

    Maybe fix RemoteDialogueTrigger softlock

---
+++ NewHorizons/Patches/RemoteDialogueTriggerPatches.cs
@@ -0,0 +1,32 @@
+using HarmonyLib;
+
+namespace NewHorizons.Patches
+{
+    /// <summary>
+    /// Should fix a bug where disabled a CharacterDialogueTree makes its related RemoteDialogueTriggers softlock your game
+    /// </summary>
+    [HarmonyPatch]
+    public static class RemoteDialogueTriggerPatches
+    {
+        private static bool _wasLastDialogueInactive = false;
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HUDMarker), nameof(HUDMarker.Awake))]
+        public static void OnTriggerEnter(RemoteDialogueTrigger __instance)
+        {
+            _wasLastDialogueInactive = __instance._activeRemoteDialogue.gameObject.activeInHierarchy;
+            __instance._activeRemoteDialogue.gameObject.SetActive(true);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HUDMarker), nameof(HUDMarker.Awake))]
+        public static void OnEndConversation(RemoteDialogueTrigger __instance)
+        {
+            if (_wasLastDialogueInactive)
+            {
+                __instance._activeRemoteDialogue.gameObject.SetActive(false);
+            }
+            _wasLastDialogueInactive = false;
+        }
+    }
+}
