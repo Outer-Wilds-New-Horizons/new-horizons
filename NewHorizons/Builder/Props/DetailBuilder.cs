cb2b8815fd3cfc973b2600ca25d0db481bad103a
JohnCorby <williamforgames6@gmail.com>
2023-02-03 18:51:21 -0800

    move to separate component

--- NewHorizons/Builder/Props/DetailBuilder.cs
+++ NewHorizons/Builder/Props/DetailBuilder.cs
@@ -1,10 +1,10 @@
 using NewHorizons.Builder.General;
+using NewHorizons.Components;
 using NewHorizons.External.Modules;
 using NewHorizons.Handlers;
 using NewHorizons.Utility;
 using OWML.Common;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -412,49 +412,6 @@ namespace NewHorizons.Builder.Props
                 angler.enabled = true;
                 angler.OnChangeAnglerState(AnglerfishController.AnglerState.Lurking);
                 
-                Destroy(this);
-            }
-        }
-
-        // detector collider is not included in groups. oh well
-        private class AddPhysics : MonoBehaviour
-        {
-            public Sector Sector;
-            public float Mass;
-            public float Radius;
-
-            private IEnumerator Start()
-            {
-                yield return new WaitForSeconds(.1f);
-
-                var parentBody = GetComponentInParent<OWRigidbody>();
-
-                // hack: make all mesh colliders convex
-                // triggers are already convex
-                // prints errors for non readable meshes but whatever
-                foreach (var meshCollider in GetComponentsInChildren<MeshCollider>(true))
-                    meshCollider.convex = true;
-
-                var bodyGo = new GameObject($"{name}_Body");
-                bodyGo.SetActive(false);
-                bodyGo.transform.position = transform.position;
-                bodyGo.transform.rotation = transform.rotation;
-
-                var owRigidbody = bodyGo.AddComponent<OWRigidbody>();
-                owRigidbody._simulateInSector = Sector;
-
-                bodyGo.layer = LayerMask.NameToLayer("PhysicalDetector");
-                bodyGo.tag = "DynamicPropDetector";
-                bodyGo.AddComponent<SphereCollider>().radius = Radius;
-                bodyGo.AddComponent<DynamicForceDetector>();
-                bodyGo.AddComponent<DynamicFluidDetector>();
-
-                bodyGo.SetActive(true);
-
-                transform.parent = bodyGo.transform;
-                owRigidbody.SetMass(Mass);
-                owRigidbody.SetVelocity(parentBody.GetPointVelocity(transform.position));
-
                 Destroy(this);
             }
         }
---
+++ NewHorizons/Components/AddPhysics.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace NewHorizons.Components
+{
+    /// <summary>
+    /// properly add physics to a detail
+    /// </summary>
+    public class AddPhysics : MonoBehaviour
+    {
+        public Sector Sector;
+        public float Mass;
+        public float Radius;
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(.1f);
+
+            var parentBody = GetComponentInParent<OWRigidbody>();
+
+            // hack: make all mesh colliders convex
+            // triggers are already convex
+            // prints errors for non readable meshes but whatever
+            foreach (var meshCollider in GetComponentsInChildren<MeshCollider>(true))
+                meshCollider.convex = true;
+
+            var bodyGo = new GameObject($"{name}_Body");
+            bodyGo.SetActive(false);
+            bodyGo.transform.position = transform.position;
+            bodyGo.transform.rotation = transform.rotation;
+
+            var owRigidbody = bodyGo.AddComponent<OWRigidbody>();
+            owRigidbody._simulateInSector = Sector;
+
+            bodyGo.layer = LayerMask.NameToLayer("PhysicalDetector");
+            bodyGo.tag = "DynamicPropDetector";
+            // this collider is not included in groups. oh well
+            bodyGo.AddComponent<SphereCollider>().radius = Radius;
+            bodyGo.AddComponent<DynamicForceDetector>();
+            bodyGo.AddComponent<DynamicFluidDetector>();
+
+            bodyGo.SetActive(true);
+
+            transform.parent = bodyGo.transform;
+            owRigidbody.SetMass(Mass);
+            owRigidbody.SetVelocity(parentBody.GetPointVelocity(transform.position));
+
+            Destroy(this);
+        }
+    }
+}
