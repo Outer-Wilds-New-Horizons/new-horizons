6e97449c2bf1d21bfccfe6210a4f305b96b51b46
JohnCorby <williamforgames6@gmail.com>
2023-02-03 18:58:34 -0800

    file scoped namespace

--- NewHorizons/Components/AddPhysics.cs
+++ NewHorizons/Components/AddPhysics.cs
@@ -1,51 +1,50 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace NewHorizons.Components
+namespace NewHorizons.Components;
+
+/// <summary>
+/// properly add physics to a detail
+/// </summary>
+public class AddPhysics : MonoBehaviour
 {
-    /// <summary>
-    /// properly add physics to a detail
-    /// </summary>
-    public class AddPhysics : MonoBehaviour
+    public Sector Sector;
+    public float Mass = 1f;
+    public float Radius = 1f;
+
+    private IEnumerator Start()
     {
-        public Sector Sector;
-        public float Mass = 1f;
-        public float Radius = 1f;
+        yield return new WaitForSeconds(.1f);
 
-        private IEnumerator Start()
-        {
-            yield return new WaitForSeconds(.1f);
+        var parentBody = GetComponentInParent<OWRigidbody>();
 
-            var parentBody = GetComponentInParent<OWRigidbody>();
+        // hack: make all mesh colliders convex
+        // triggers are already convex
+        // prints errors for non readable meshes but whatever
+        foreach (var meshCollider in GetComponentsInChildren<MeshCollider>(true))
+            meshCollider.convex = true;
 
-            // hack: make all mesh colliders convex
-            // triggers are already convex
-            // prints errors for non readable meshes but whatever
-            foreach (var meshCollider in GetComponentsInChildren<MeshCollider>(true))
-                meshCollider.convex = true;
+        var bodyGo = new GameObject($"{name}_Body");
+        bodyGo.SetActive(false);
+        bodyGo.transform.position = transform.position;
+        bodyGo.transform.rotation = transform.rotation;
 
-            var bodyGo = new GameObject($"{name}_Body");
-            bodyGo.SetActive(false);
-            bodyGo.transform.position = transform.position;
-            bodyGo.transform.rotation = transform.rotation;
+        var owRigidbody = bodyGo.AddComponent<OWRigidbody>();
+        owRigidbody._simulateInSector = Sector;
 
-            var owRigidbody = bodyGo.AddComponent<OWRigidbody>();
-            owRigidbody._simulateInSector = Sector;
+        bodyGo.layer = LayerMask.NameToLayer("PhysicalDetector");
+        bodyGo.tag = "DynamicPropDetector";
+        // this collider is not included in groups. oh well
+        bodyGo.AddComponent<SphereCollider>().radius = Radius;
+        bodyGo.AddComponent<DynamicForceDetector>();
+        bodyGo.AddComponent<DynamicFluidDetector>();
 
-            bodyGo.layer = LayerMask.NameToLayer("PhysicalDetector");
-            bodyGo.tag = "DynamicPropDetector";
-            // this collider is not included in groups. oh well
-            bodyGo.AddComponent<SphereCollider>().radius = Radius;
-            bodyGo.AddComponent<DynamicForceDetector>();
-            bodyGo.AddComponent<DynamicFluidDetector>();
+        bodyGo.SetActive(true);
 
-            bodyGo.SetActive(true);
+        transform.parent = bodyGo.transform;
+        owRigidbody.SetMass(Mass);
+        owRigidbody.SetVelocity(parentBody.GetPointVelocity(transform.position));
 
-            transform.parent = bodyGo.transform;
-            owRigidbody.SetMass(Mass);
-            owRigidbody.SetVelocity(parentBody.GetPointVelocity(transform.position));
-
-            Destroy(this);
-        }
+        Destroy(this);
     }
 }
