bd61a87cf62310ee438d86d908f469d7892f1b3d
Nick <nicholasconnors@hotmail.ca>
2023-03-18 16:04:53 -0400

    Add game over text and death type to credits volume, but doesnt work

--- NewHorizons/Components/Volumes/LoadCreditsVolume.cs
+++ NewHorizons/Components/Volumes/LoadCreditsVolume.cs
@@ -1,4 +1,6 @@
 using NewHorizons.External.Modules;
+using NewHorizons.Handlers;
+using System.Collections;
 using UnityEngine;
 
 namespace NewHorizons.Components.Volumes
@@ -7,29 +9,74 @@ namespace NewHorizons.Components.Volumes
     {
         public VolumesModule.LoadCreditsVolumeInfo.CreditsType creditsType = VolumesModule.LoadCreditsVolumeInfo.CreditsType.Fast;
 
+        public string gameOverText;
+        public DeathType deathType = DeathType.Default;
+
+        private GameOverController _gameOverController;
+        private PlayerCameraEffectController _playerCameraEffectController;
+
+        public void Start()
+        {
+            _gameOverController = GameObject.FindObjectOfType<GameOverController>();
+            _playerCameraEffectController = GameObject.FindObjectOfType<PlayerCameraEffectController>();
+        }
+
         public override void OnTriggerVolumeEntry(GameObject hitObj)
         {
             if (hitObj.CompareTag("PlayerDetector") && enabled)
             {
-                switch(creditsType)
-                {
-                    case VolumesModule.LoadCreditsVolumeInfo.CreditsType.Fast:
-                        LoadManager.LoadScene(OWScene.Credits_Fast, LoadManager.FadeType.ToBlack);
-                        break;
-                    case VolumesModule.LoadCreditsVolumeInfo.CreditsType.Final:
-                        LoadManager.LoadScene(OWScene.Credits_Final, LoadManager.FadeType.ToBlack);
-                        break;
-                    case VolumesModule.LoadCreditsVolumeInfo.CreditsType.Kazoo:
-                        TimelineObliterationController.s_hasRealityEnded = true;
-                        LoadManager.LoadScene(OWScene.Credits_Fast, LoadManager.FadeType.ToBlack);
-                        break;
-                }
+                StartCoroutine(GameOver());
             }
         }
 
-        public override void OnTriggerVolumeExit(GameObject hitObj)
+        private IEnumerator GameOver()
         {
+            OWInput.ChangeInputMode(InputMode.None);
+            ReticleController.Hide();
+            Locator.GetPromptManager().SetPromptsVisible(false);
+            Locator.GetPauseCommandListener().AddPauseCommandLock();
 
+            _playerCameraEffectController.OnPlayerDeath(deathType);
+            
+            yield return new WaitForSeconds(_playerCameraEffectController._deathFadeLength);
+
+            if (!string.IsNullOrEmpty(gameOverText) && _gameOverController != null)
+            {
+                _gameOverController._deathText.text = TranslationHandler.GetTranslation(gameOverText, TranslationHandler.TextType.UI);
+                _gameOverController.SetupGameOverScreen(5f);
+
+                // We set this to true to stop it from loading the credits scene, so we can do it ourselves
+                _gameOverController._loading = true;
+
+                yield return new WaitUntil(ReadytoLoadCreditsScene);
+
+                LoadCreditsScene();
+            }
+            else
+            {
+                LoadCreditsScene();
+            }
+        }
+
+        private bool ReadytoLoadCreditsScene() => _gameOverController._fadedOutText && _gameOverController._textAnimator.IsComplete();
+
+        public override void OnTriggerVolumeExit(GameObject hitObj) { }
+
+        private void LoadCreditsScene()
+        {
+            switch (creditsType)
+            {
+                case VolumesModule.LoadCreditsVolumeInfo.CreditsType.Fast:
+                    LoadManager.LoadScene(OWScene.Credits_Fast, LoadManager.FadeType.ToBlack);
+                    break;
+                case VolumesModule.LoadCreditsVolumeInfo.CreditsType.Final:
+                    LoadManager.LoadScene(OWScene.Credits_Final, LoadManager.FadeType.ToBlack);
+                    break;
+                case VolumesModule.LoadCreditsVolumeInfo.CreditsType.Kazoo:
+                    TimelineObliterationController.s_hasRealityEnded = true;
+                    LoadManager.LoadScene(OWScene.Credits_Fast, LoadManager.FadeType.ToBlack);
+                    break;
+            }
         }
     }
 }
