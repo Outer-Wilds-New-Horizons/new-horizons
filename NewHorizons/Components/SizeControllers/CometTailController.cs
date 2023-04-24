d2f6d0b52e80ba4d255d483c93ae6ee2b943e9d1
JohnCorby <williamforgames6@gmail.com>
2023-04-23 17:46:29 -0700

    TODO

--- NewHorizons/Components/SizeControllers/CometTailController.cs
+++ NewHorizons/Components/SizeControllers/CometTailController.cs
@@ -40,6 +40,7 @@ namespace NewHorizons.Components.SizeControllers
         private void UpdateTargetPositions()
         {
             // body is null for proxies
+            // TODO: this will make proxy tails face the real body rather than proxy body (ie wrong). fix properly in a different PR
             var toPrimary = ((_body ? _body.transform : transform).position - _dustTargetBody.transform.position).normalized;
             var velocityDirection = (_primaryBody?.GetVelocity() ?? Vector3.zero) - (_body ? _body.GetVelocity() : Vector3.zero); // Accept that this is flipped ok
 
