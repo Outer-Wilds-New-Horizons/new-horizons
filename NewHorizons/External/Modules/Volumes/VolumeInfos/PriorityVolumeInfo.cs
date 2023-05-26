2ce361e182213fd3286c9ecbbd3ba894ebc941c0
JohnCorby <williamforgames6@gmail.com>
2023-05-26 13:31:41 -0700

    priority doc

--- NewHorizons/External/Modules/Volumes/VolumeInfos/PriorityVolumeInfo.cs
+++ NewHorizons/External/Modules/Volumes/VolumeInfos/PriorityVolumeInfo.cs
@@ -14,6 +14,8 @@ namespace NewHorizons.External.Modules.Volumes.VolumeInfos
         /// <summary>
         /// The priority for this volume's effects to be applied. 
         /// Ex, a player in a gravity volume with priority 0, and zero-gravity volume with priority 1, will feel zero gravity.
+        ///
+        /// Default value here is 1 instead of 0 so it automatically overrides planet gravity, which is 0 by default. 
         /// </summary>
         [DefaultValue(1)] public int priority = 1;
     }
