a7557ac22d07dcdfd0305303ea075db1156b09d6
JohnCorby <williamforgames6@gmail.com>
2023-05-26 15:42:29 -0700

    bleh

--- NewHorizons/External/Modules/Volumes/VolumeInfos/PriorityVolumeInfo.cs
+++ NewHorizons/External/Modules/Volumes/VolumeInfos/PriorityVolumeInfo.cs
@@ -14,7 +14,7 @@ namespace NewHorizons.External.Modules.Volumes.VolumeInfos
         ///
         /// Ex: A player could be affected by the sun on layer 9 priority 0 and planet gravity on layer 3 priority 2. They would experience the gravity of both volumes since they are on different layers.
         /// If there was a zero-g volume on layer 0 priority 1, since it is on layer 0 it will override the gravity from the sun (priority 0 which is less than 1) but they will still feel the 
-        /// gravity of the planet (priority 2 is greater than 1).
+        /// gravity of the planet (priority 2 is greater than 1). The zero-g volume will also still be applied because it is on a different layer.
         ///
         /// Default value here is 0 which means this volume's priority will be evaluated against all other priority volumes regardless of their layer.
         /// </summary>
