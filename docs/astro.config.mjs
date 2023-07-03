278f775a381427c68d79ea56635c3ab7293f2585
Ben C <bwc9876@gmail.com>
2023-07-03 02:56:30 -0400

    Add Sitemaps, Robots

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,17 +1,25 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+const site = `https://nh.outerwildsmods.com`;
 
 // https://astro.build/config
 export default defineConfig({
+    compressHTML: true,
     integrations: [
         // Enable Preact to support Preact JSX components.
         preact(),
         // Enable React for the Algolia search component.
         react(),
-        mdx()
+        mdx(),
+        sitemap({
+            filter: (page) =>
+                page !== `${site}/404.html` && page !== `${site}/reference/audio-enum/`
+        })
     ],
-    site: `https://nh.outerwildsmods.com`
+    site
 });
