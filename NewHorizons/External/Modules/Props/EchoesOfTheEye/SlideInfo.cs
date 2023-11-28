67ff544fb38eec9d4d7cf58a2aab22b0b903e37a
JohnCorby <williamforgames6@gmail.com>
2023-11-28 13:11:04 -0800

    document base game defaults for slides

--- NewHorizons/External/Modules/Props/EchoesOfTheEye/SlideInfo.cs
+++ NewHorizons/External/Modules/Props/EchoesOfTheEye/SlideInfo.cs
@@ -7,80 +7,75 @@ namespace NewHorizons.External.Modules.Props.EchoesOfTheEye
     public class SlideInfo
     {
         /// <summary>
-        /// Ambient light colour when viewing this slide.
+        /// The path to the image file for this slide.
         /// </summary>
-        public MColor ambientLightColor;
-
+        public string imagePath;
 
         // SlideAmbientLightModule
 
         /// <summary>
-        /// Ambient light intensity when viewing this slide.
+        /// Ambient light intensity when viewing this slide. (Base game default: 1)
         /// </summary>
         public float ambientLightIntensity;
 
         /// <summary>
-        /// Ambient light range when viewing this slide.
+        /// Ambient light range when viewing this slide. (Base game default: 20)
         /// </summary>
         public float ambientLightRange;
+
+        /// <summary>
+        /// Ambient light colour when viewing this slide. (Base game default: white)
+        /// </summary>
+        public MColor ambientLightColor;
+
+        /// <summary>
+        /// Spotlight intensity modifier when viewing this slide. (Base game default: 0)
+        /// </summary>
+        public float spotIntensityMod;
 
         // SlideBackdropAudioModule
 
         /// <summary>
-        /// The name of the AudioClip that will continuously play while watching these slides
+        /// The name of the AudioClip that will continuously play while watching these slides (Base game default: Reel_1_Backdrop_A)
         /// </summary>
         public string backdropAudio;
 
         /// <summary>
-        /// The time to fade into the backdrop audio
+        /// The time to fade into the backdrop audio (Base game default: 2)
         /// </summary>
         public float backdropFadeTime;
 
         // SlideBeatAudioModule
 
         /// <summary>
-        /// The name of the AudioClip for a one-shot sound when opening the slide.
+        /// The name of the AudioClip for a one-shot sound when opening the slide. (Base game default: Reel_1_Beat_A)
         /// </summary>
         public string beatAudio;
 
         /// <summary>
-        /// The time delay until the one-shot audio
+        /// The time delay until the one-shot audio (Base game default: 0)
         /// </summary>
         public float beatDelay;
-
 
         // SlideBlackFrameModule
 
         /// <summary>
-        /// Before viewing this slide, there will be a black frame for this many seconds.
+        /// Before viewing this slide, there will be a black frame for this many seconds. (Base game default: 0)
         /// </summary>
         public float blackFrameDuration;
-
-        /// <summary>
-        /// The path to the image file for this slide.
-        /// </summary>
-        public string imagePath;
-
 
         // SlidePlayTimeModule
 
         /// <summary>
-        /// Play-time duration for auto-projector slides.
+        /// Play-time duration for auto-projector slides. (Base game default: 0)
         /// </summary>
         public float playTimeDuration;
-
 
         // SlideShipLogEntryModule
 
         /// <summary>
-        /// Ship log fact revealed when viewing this slide
+        /// Ship log fact revealed when viewing this slide (Base game default: "")
         /// </summary>
         public string reveal;
-
-        /// <summary>
-        /// Spotlight intensity modifier when viewing this slide.
-        /// </summary>
-        public float spotIntensityMod;
     }
-
 }
