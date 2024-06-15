e78a892bf49329d8b4147dc9e298b72129ebcc15
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-06-14 21:50:09 -0400

    Add display names to eye astro objects

--- NewHorizons/Patches/MapPatches/ReferenceFramePatches.cs
+++ NewHorizons/Patches/MapPatches/ReferenceFramePatches.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using NewHorizons.Components.EyeOfTheUniverse;
 using NewHorizons.Components.Orbital;
 using NewHorizons.Handlers;
 
@@ -11,7 +12,27 @@ namespace NewHorizons.Patches.MapPatches
         [HarmonyPatch(nameof(ReferenceFrame.GetHUDDisplayName))]
         public static void ReferenceFrame_GetHUDDisplayName(ReferenceFrame __instance, ref string __result)
         {
-            if (__instance.GetAstroObject() is NHAstroObject nhao && !nhao.isVanilla && !nhao.HideDisplayName)
+            var ao = __instance.GetAstroObject();
+
+            if (ao is EyeAstroObject eyeao && !eyeao.HideDisplayName)
+            {
+                var name = eyeao.GetAstroObjectName();
+
+                if (name == AstroObject.Name.Eye)
+                {
+                    __result = UITextLibrary.GetString(UITextType.LocationEye);
+                }
+                else
+                {
+                    var customName = eyeao.GetCustomName();
+
+                    if (!string.IsNullOrWhiteSpace(customName))
+                    {
+                        __result = TranslationHandler.GetTranslation(customName, TranslationHandler.TextType.UI, false);
+                    }
+                }
+            }
+            else if (ao is NHAstroObject nhao && !nhao.isVanilla && !nhao.HideDisplayName)
             {
                 var customName = nhao.GetCustomName();
 
