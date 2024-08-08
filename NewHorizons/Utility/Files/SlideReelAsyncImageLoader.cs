2b3b7e277eb080fa9033d5b8d0b3faf327a44284
xen-42 <nicholasconnors@hotmail.ca>
2024-08-08 11:50:21 -0400

    Update SlideReelAsyncImageLoader.cs

--- NewHorizons/Utility/Files/SlideReelAsyncImageLoader.cs
+++ NewHorizons/Utility/Files/SlideReelAsyncImageLoader.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
