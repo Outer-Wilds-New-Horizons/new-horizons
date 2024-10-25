c566aa40750691b66174af0e607f20733e8e8bb1
xen-42 <nicholasconnors@hotmail.ca>
2024-10-25 15:39:11 -0400

    what if i do this

--- NewHorizons/External/SerializableEnums/NHDeathType.cs
+++ NewHorizons/External/SerializableEnums/NHDeathType.cs
@@ -4,6 +4,19 @@ using System.Runtime.Serialization;
 
 namespace NewHorizons.External.SerializableEnums
 {
+    /// <summary>
+    /// Some special death types are:
+    /// 
+    /// Supernova - Special death type used when the supernova hits you. You will not wake up if in the Dreamworld. 
+    /// Digestion - Death type used by anglerfish (and cut-content ghosts and water monster)
+    /// Big bang - Special death type used at the end of the game
+    /// Meditation - Special death type used when skipping to the next loop. You will not wake up if in the Dreamworld. 
+    /// Timeloop - Special death type used when the time loop ends. You will not wake up if in the Dreamworld. 
+    /// Blackhole - Special death type used by the ATP blackhole (and custom NH blackholes without whitehole destinations)
+    /// Dream - Special DLC death type used to kill a player in the real world while in the Dreamworld (i.e., you will loop not wake up)
+    /// DreamExplosion - Special DLC death type used by the prototype artifact to kill a player in the real world while in the Dreamworld (i.e., you will loop not wake up)
+    /// Similar to the Crushed death type, but much faster
+    /// </summary>
     [JsonConverter(typeof(StringEnumConverter))]
     public enum NHDeathType
     {
@@ -11,53 +24,16 @@ namespace NewHorizons.External.SerializableEnums
         [EnumMember(Value = @"impact")] Impact,
         [EnumMember(Value = @"asphyxiation")] Asphyxiation,
         [EnumMember(Value = @"energy")] Energy,
-
-        /// <summary>
-        /// Special death type used when the supernova hits you. You will not wake up if in the Dreamworld. 
-        /// </summary>
         [EnumMember(Value = @"supernova")] Supernova,
-
-        /// <summary>
-        /// Death type used by anglerfish (and cut-content ghosts and water monster)
-        /// </summary>
         [EnumMember(Value = @"digestion")] Digestion,
-
-        /// <summary>
-        /// Special death type used at the end of the game
-        /// </summary>
         [EnumMember(Value = @"bigBang")] BigBang,
         [EnumMember(Value = @"crushed")] Crushed,
-
-        /// <summary>
-        /// Special death type used when skipping to the next loop. You will not wake up if in the Dreamworld. 
-        /// </summary>
         [EnumMember(Value = @"meditation")] Meditation,
-
-        /// <summary>
-        /// Special death type used when the time loop ends. You will not wake up if in the Dreamworld. 
-        /// </summary>
         [EnumMember(Value = @"timeLoop")] TimeLoop,
-
         [EnumMember(Value = @"lava")] Lava,
-
-        /// <summary>
-        /// Special death type used by the ATP blackhole (and custom NH blackholes without whitehole destinations)
-        /// </summary>
         [EnumMember(Value = @"blackHole")] BlackHole,
-
-        /// <summary>
-        /// Special DLC death type used to kill a player in the real world while in the Dreamworld (i.e., you will loop not wake up)
-        /// </summary>
         [EnumMember(Value = @"dream")] Dream,
-
-        /// <summary>
-        /// Special DLC death type used to kill a player in the real world while in the Dreamworld (i.e., you will loop not wake up)
-        /// </summary>
         [EnumMember(Value = @"dreamExplosion")] DreamExplosion,
-
-        /// <summary>
-        /// Similar to the Crushed death type, but much faster
-        /// </summary>
         [EnumMember(Value = @"crushedByElevator")] CrushedByElevator
     }
 }
