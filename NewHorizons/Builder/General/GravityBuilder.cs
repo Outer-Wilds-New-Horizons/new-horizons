ff7c72ab0349925847819996d496135496390f6a
Noah Pilarski <megapiggy@outerwildsmods.com>
2025-01-06 15:18:51 -0500

    make focal gravity radius 0
    i have no idea why i did 1 before as there is no reason to

--- NewHorizons/Builder/General/GravityBuilder.cs
+++ NewHorizons/Builder/General/GravityBuilder.cs
@@ -15,7 +15,7 @@ namespace NewHorizons.Builder.General
             var gravityRadius = GM / 0.1f;
             if (exponent == 2f) gravityRadius = Mathf.Sqrt(gravityRadius);
 
-            if (config.FocalPoint != null) gravityRadius = 1; // keep it at the lowest possible
+            if (config.FocalPoint != null) gravityRadius = 0; // keep it at the lowest possible
             else if (config.Base.soiOverride != 0f) gravityRadius = config.Base.soiOverride;
             else if (config.Star != null) gravityRadius = Mathf.Min(gravityRadius, 15 * config.Base.surfaceSize);
             // To let you actually orbit things the way you would expect we cap this at 4x the diameter if its not a star (this is what giants deep has)
@@ -49,7 +49,7 @@ namespace NewHorizons.Builder.General
             if (config.Base.surfaceGravity == 0) alignmentRadius = 0;
 
             gravityVolume._alignmentRadius = config.Base.gravityAlignmentRadiusOverride ?? alignmentRadius;
-            gravityVolume._upperSurfaceRadius = config.Base.surfaceSize;
+            gravityVolume._upperSurfaceRadius = config.FocalPoint != null ? 0 : config.Base.surfaceSize;
             gravityVolume._lowerSurfaceRadius = 0;
             gravityVolume._layer = 3;
             gravityVolume._priority = config.Base.gravityVolumePriority;
