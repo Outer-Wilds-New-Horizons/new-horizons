ed4e43a04484d1ce5b1724522385358e5942c996
JohnCorby <williamforgames6@gmail.com>
2025-01-13 16:49:28 -0800

    delete duplicate box shape fixer

--- NewHorizons/Builder/Props/QuantumBuilder.cs
+++ NewHorizons/Builder/Props/QuantumBuilder.cs
@@ -295,39 +295,4 @@ namespace NewHorizons.Builder.Props
             return globalCorners.Select(globalCorner => relativeTo.transform.InverseTransformPoint(globalCorner)).ToArray();
         }
     }
-
-    /// <summary>
-    /// for some reason mesh bounds are wrong unless we wait a bit
-    /// so this script contiously checks everything until it is correct
-    ///
-    /// this actually only seems to be a problem with skinned renderers. normal ones work fine
-    /// TODO: at some point narrow this down to just skinned, instead of doing everything and checking every frame
-    /// </summary>
-    public class BoxShapeFixer : MonoBehaviour
-    {
-        public BoxShape shape;
-        public MeshFilter meshFilter;
-        public SkinnedMeshRenderer skinnedMeshRenderer;
-
-        public void Update()
-        {
-            if (meshFilter == null && skinnedMeshRenderer == null)
-            {
-                NHLogger.LogVerbose("Useless BoxShapeFixer, destroying");
-                DestroyImmediate(this);
-            }
-
-            Mesh sharedMesh = null;
-            if (meshFilter != null) sharedMesh = meshFilter.sharedMesh;
-            if (skinnedMeshRenderer != null) sharedMesh = skinnedMeshRenderer.sharedMesh;
-
-            if (sharedMesh == null) return;
-            if (sharedMesh.bounds.size == Vector3.zero) return;
-
-            shape.size = sharedMesh.bounds.size;
-            shape.center = sharedMesh.bounds.center;
-
-            DestroyImmediate(this);
-        }
-    }
 }
--- NewHorizons/Utility/Geometry/BoxShapeFinder.cs
+++ NewHorizons/Utility/Geometry/BoxShapeFinder.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 
 namespace NewHorizons.Utility.Geometry;
 
-internal class BoxShapeFixer : MonoBehaviour
+/// <summary>
+/// for some reason mesh bounds are wrong unless we wait a bit
+/// so this script contiously checks everything until it is correct
+///
+/// this actually only seems to be a problem with skinned renderers. normal ones work fine
+/// TODO: at some point narrow this down to just skinned, instead of doing everything and checking every frame
+/// </summary>
+public class BoxShapeFixer : MonoBehaviour
 {
     public BoxShape shape;
     public MeshFilter meshFilter;
@@ -13,27 +20,16 @@ internal class BoxShapeFixer : MonoBehaviour
     {
         if (meshFilter == null && skinnedMeshRenderer == null)
         {
-            NHLogger.LogVerbose("Useless BoxShapeFixer, destroying"); DestroyImmediate(this);
+            NHLogger.LogVerbose("Useless BoxShapeFixer, destroying");
+            DestroyImmediate(this);
         }
 
         Mesh sharedMesh = null;
-        if (meshFilter != null)
-        {
-            sharedMesh = meshFilter.sharedMesh;
-        }
-        if (skinnedMeshRenderer != null)
-        {
-            sharedMesh = skinnedMeshRenderer.sharedMesh;
-        }
+        if (meshFilter != null) sharedMesh = meshFilter.sharedMesh;
+        if (skinnedMeshRenderer != null) sharedMesh = skinnedMeshRenderer.sharedMesh;
 
-        if (sharedMesh == null)
-        {
-            return;
-        }
-        if (sharedMesh.bounds.size == Vector3.zero)
-        {
-            return;
-        }
+        if (sharedMesh == null) return;
+        if (sharedMesh.bounds.size == Vector3.zero) return;
 
         shape.size = sharedMesh.bounds.size;
         shape.center = sharedMesh.bounds.center;
