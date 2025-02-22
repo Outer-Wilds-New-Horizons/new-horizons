2c7b5a63f6c626c167038ab788afa38e7f6b364b
Joshua Thome <joshpthome@gmail.com>
2025-02-21 22:10:54 -0600

    Disable nomai/translator text scroll colliders

--- NewHorizons/Builder/Props/NomaiTextBuilder.cs
+++ NewHorizons/Builder/Props/NomaiTextBuilder.cs
@@ -312,7 +312,7 @@ namespace NewHorizons.Builder.Props
                                 scrollItem._nomaiWallText = nomaiWallText;
                                 scrollItem.SetSector(sector);
                                 customScroll.transform.Find("Props_NOM_Scroll/Props_NOM_Scroll_Geo").GetComponent<MeshRenderer>().enabled = true;
-                                customScroll.transform.Find("Props_NOM_Scroll/Props_NOM_Scroll_Collider").gameObject.SetActive(true);
+                                customScroll.transform.Find("Props_NOM_Scroll/Props_NOM_Scroll_Collider").gameObject.SetActive(false);
                                 nomaiWallText.gameObject.GetComponent<Collider>().enabled = false;
                                 customScroll.GetComponent<CapsuleCollider>().enabled = true;
                             }
