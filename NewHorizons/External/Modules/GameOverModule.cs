a6efef5ee392aee84f81ece1a9e71813acd6a20a
xen-42 <nicholasconnors@hotmail.ca>
2025-04-08 13:03:55 -0400

    Reword

--- NewHorizons/External/Modules/GameOverModule.cs
+++ NewHorizons/External/Modules/GameOverModule.cs
@@ -22,10 +22,10 @@ namespace NewHorizons.External.Modules
         /// Condition that must be true for this game over to trigger. If this is on a LoadCreditsVolume, leave empty to always trigger this game over.
         /// Note this is a regular dialogue condition, not a persistent condition.
         /// </summary>
-        public string condition;    
+        public string condition;
 
         /// <summary>
-        /// Path to the audio file to use as custom music for the credits. Also supports AudioClips and AudioType like other audio settings.
+        /// The audio to use for the credits music. Can be a path to a .wav/.ogg/.mp3 file, or taken from the AudioClip list.
         /// Note: only applies when creditsType is set to "custom".
         /// </summary>
         public string audio;
