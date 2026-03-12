ecc6708eab2b298371a0dcdf269a6cf9f2de3d89
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-03-12 05:45:42 -0400

    log

--- NewHorizons/Utility/OWML/EnumUtilities.cs
+++ NewHorizons/Utility/OWML/EnumUtilities.cs
@@ -10,6 +10,7 @@ namespace NewHorizons.Utility.OWML
 
         public static void ClearCache()
         {
+            NHLogger.LogVerbose($"Clearing enum cache");
             foreach (var @enum in Enums)
             {
                 if (@enum == null) continue;
@@ -20,6 +21,7 @@ namespace NewHorizons.Utility.OWML
 
         public static T Create<T>(string name) where T : Enum
         {
+            NHLogger.LogVerbose($"Creating enum value [{name}] for type [{typeof(T).FullName}]");
             T @enum = EnumUtils.Create<T>(name);
             Enums.SafeAdd(@enum);
             return @enum;
@@ -27,12 +29,14 @@ namespace NewHorizons.Utility.OWML
 
         public static void Create<T>(string name, T value) where T : Enum
         {
+            NHLogger.LogVerbose($"Creating enum value [{name}] for type [{typeof(T).FullName}]");
             EnumUtils.Create(name, value);
             Enums.SafeAdd(value);
         }
 
         public static void Remove<T>(string name) where T : Enum
         {
+            NHLogger.LogVerbose($"Removing enum value [{name}] from type [{typeof(T).FullName}]");
             T @enum = EnumUtils.Parse<T>(name);
             Enums.Remove(@enum);
             EnumUtils.Remove<T>(name);
@@ -40,6 +44,7 @@ namespace NewHorizons.Utility.OWML
 
         public static void Remove<T>(T value) where T : Enum
         {
+            NHLogger.LogVerbose($"Removing enum value [{value.GetName()}] from type [{typeof(T).FullName}]");
             Enums.Remove(value);
             EnumUtils.Remove<T>(value);
         }
